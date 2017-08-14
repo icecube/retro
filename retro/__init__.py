making hypo_vector and hypo_fast more consistent; sped up latter; introduced bug in computation though which makes the two differ more

retro/__init__.py
@@ -3,57 +3,134 @@ Basic module-wide definitions and simple types (namedtuples).
 """
 
 
-from collections import namedtuple
+from __future__ import absolute_import, division, print_function
 
+from collections import namedtuple
+from os.path import abspath, expanduser, expandvars
+
+import h5py
 import numpy as np
 
-
-__all__ = ['FTYPE', 'SPEED_OF_LIGHT_M_PER_NS', 'TWO_PI', 'PI_BY_TWO',
-           'HypoParams8D', 'HypoParams10D', 'HYPO_PARAMS_T', 'TrackParams',
-           'Event', 'Pulses', 'PhotonInfo', 'BinningCoords',
-           'event_to_hypo_params', 'hypo_to_track_params', 'power_axis',
-           'Events']
+from particles import ParticleArray
 
 
-FTYPE = np.float32
+__all__ = ['FTYPE', 'UITYPE', 'DFLT_PULSE_SERIES', 'DFLT_ML_RECO_NAME',
+           'DFLT_SPE_RECO_NAME', 'SPEED_OF_LIGHT_M_PER_NS', 'TWO_PI',
+           'PI_BY_TWO', 'HypoParams8D', 'HypoParams10D', 'HYPO_PARAMS_T',
+           'TrackParams', 'Event', 'Pulses', 'PhotonInfo', 'BinningCoords',
+           'TimeSpaceCoord', 'expand', 'event_to_hypo_params',
+           'hypo_to_track_params', 'power_axis', 'Events']
+
+
+# -- Datatypes to use -- #
+
+FTYPE = np.float64
+"""Datatype to use for explicitly-typed floating point numbers"""
+
+UITYPE = np.uint64
+"""Datatype to use for explicitly-typed unsigned integers"""
+
+# -- Default choices we've made -- #
+
+DFLT_PULSE_SERIES = 'SRTInIcePulses'
+"""Default pulse series to extract from events"""
+
+DFLT_ML_RECO_NAME = 'IC86_Dunkman_L6_PegLeg_MultiNest8D_NumuCC'
+"""Default maximum-likelihood reco to extract for an event"""
+
+DFLT_SPE_RECO_NAME = 'SPEFit2'
+"""Default single photoelectron (SPE) reco to extract for an event"""
+
+# -- Physical / mathematical constants -- #
+
+TWO_PI = FTYPE(2*np.pi)
+"""2 * pi"""
+
+PI_BY_TWO = FTYPE(np.pi / 2)
+"""pi / 2"""
 
 SPEED_OF_LIGHT_M_PER_NS = FTYPE(0.299792458)
 """Speed of light in units of m/ns"""
 
-TWO_PI = FTYPE(2*np.pi)
-PI_BY_TWO = FTYPE(np.pi / 2)
+# -- Precalculated (using ``nphotons.py``) to avoid icetray -- #
 
-HypoParams8D = namedtuple(typename='HypoParams8D', # pylint: disable=invalid-name
-                          field_names=('t', 'x', 'y', 'z', 'track_zenith',
-                                       'track_azimuth', 'track_energy',
-                                       'cascade_energy'))
+TRACK_M_PER_GEV = 15 / 3.3
+"""Track length per energy, in units of m/GeV"""
 
-HypoParams10D = namedtuple(typename='HypoParams10D', # pylint: disable=invalid-name
-                           field_names=('t', 'x', 'y', 'z', 'track_azimuth',
-                                        'track_zenith', 'track_energy',
-                                        'cascade_azimuth', 'cascade_zenith',
-                                        'cascade_energy'))
+TRACK_PHOTONS_PER_M = 2451.4544553
+"""Track photons per length, in units of 1/m"""
+
+CASCADE_PHOTONS_PER_GEV = 12805.3383311
+"""Cascade photons per energy, in units of 1/GeV"""
+
+# -- namedtuples for interface simplicity and consistency -- #
+
+HypoParams8D = namedtuple( # pylint: disable=invalid-name
+    typename='HypoParams8D',
+    field_names=('t', 'x', 'y', 'z', 'track_zenith', 'track_azimuth',
+                 'track_energy', 'cascade_energy')
+)
+"""Hypothesis in 8 dimensions (parameters)"""
+
+HypoParams10D = namedtuple( # pylint: disable=invalid-name
+    typename='HypoParams10D',
+    field_names=(HypoParams8D._fields + ('cascade_zenith', 'cascade_azimuth'))
+)
+"""Hypothesis in 10 dimensions (parameters)"""
+
+TrackParams = namedtuple( # pylint: disable=invalid-name
+    typename='TrackParams',
+    field_names=('t', 'x', 'y', 'z', 'zenith', 'azimuth', 'energy')
+)
+"""Hypothesis for just the track (7 dimensions / parameters)"""
 
 HYPO_PARAMS_T = HypoParams8D
+"""Global selection of which hypothesis to use throughout the code."""
 
-TrackParams = namedtuple(typename='TrackParams', # pylint: disable=invalid-name
-                         field_names=('t', 'x', 'y', 'z', 'azimuth', 'zenith',
-                                      'length'))
+Event = namedtuple( # pylint: disable=invalid-name
+    typename='Event',
+    field_names=('event', 'pulses', 'interaction', 'neutrino', 'track',
+                 'cascade', 'ml_reco', 'spe_reco')
+)
 
+Pulses = namedtuple( # pylint: disable=invalid-name
+    typename='Pulses',
+    field_names=('strings', 'oms', 'times', 'charges')
+)
 
-Event = namedtuple(typename='Event', # pylint: disable=invalid-name
-                   field_names=('event', 'pulses', 'interaction', 'neutrino',
-                                'track', 'cascade', 'ml_reco', 'spe_reco'))
-
-Pulses = namedtuple(typename='Pulses', # pylint: disable=invalid-name
-                    field_names=('strings', 'oms', 'times', 'charges'))
-
-PhotonInfo = namedtuple(typename='PhotonInfo', # pylint: disable=invalid-name
-                        field_names=('count', 'theta', 'phi', 'length'))
+PhotonInfo = namedtuple( # pylint: disable=invalid-name
+    typename='PhotonInfo',
+    field_names=('count', 'theta', 'phi', 'length')
+)
 """Intended to contain dictionaries with DOM depth number as keys"""
 
-BinningCoords = namedtuple(typename='BinningCoords', # pylint: disable=invalid-name
-                           field_names=('t', 'r', 'theta', 'phi'))
+BinningCoords = namedtuple( # pylint: disable=invalid-name
+    typename='BinningCoords',
+    field_names=('t', 'r', 'theta', 'phi')
+)
+"""Binning coordinates."""
+
+TimeSpaceCoord = namedtuple( # pylint: disable=invalid-name
+    typename='TimeSpaceCoord',
+    field_names=('t', 'x', 'y', 'z'))
+"""Time and space coordinates: t, x, y, z."""
+
+
+def expand(p):
+    """Fully expand a path.
+
+    Parameters
+    ----------
+    p : string
+        Path to expand
+
+    Returns
+    -------
+    e : string
+        Expanded path
+
+    """
+    return abspath(expanduser(expandvars(p)))
 
 
 def event_to_hypo_params(event):
@@ -115,19 +192,81 @@ def hypo_to_track_params(hypo_params):
         z=hypo_params.z,
         zenith=hypo_params.track_zenith,
         azimuth=hypo_params.track_azimuth,
-        length=FTYPE(15 / 3.3 * hypo_params.track_energy)
+        energy=hypo_params.track_energy
     )
     return track_params
 
 
-def power_axis(minval, maxval, n_bins, power):
-    """JVS's power axis, reverse engeneered"""
-    inv_power = 1/power
-    liner_edges = np.linspace(np.power(minval, inv_power),
-                              np.power(maxval, inv_power),
-                              n_bins + 1)
+def power_axis(start, stop, num_bins, power):
+    """Create bin edges evenly spaced w.r.t. ``x**power``.
+
+    Reverse engineered from JVS's power axis.
+
+    Parameters
+    ----------
+    start : float
+        Lower-most bin edge
+
+    stop : float
+        Upper-most bin edge
+
+    num_bins : int
+        Number of bins (there are num_bins + 1 edges)
+
+    power : float
+        Power-law to use for even spacing
+
+    Returns
+    -------
+    edges : numpy.ndarray of shape (1, num_bins)
+        Bin edges
+
+    """
+    inv_power = 1 / power
+    liner_edges = np.linspace(np.power(start, inv_power),
+                              np.power(stop, inv_power),
+                              num_bins + 1)
     bin_edges = np.power(liner_edges, power)
     return bin_edges
+
+
+def binspec_to_edges(start, stop, num_bins):
+    """Convert binning specification (start, stop, and num_bins) to bin edges.
+
+    Note:
+    * t-bins are linearly spaced in ``t``
+    * r-bins are evenly spaced w.r.t. ``r**2``
+    * theta-bins are evenly spaced w.r.t. ``cos(theta)``
+    * phi bins are linearly spaced in ``phi``
+
+    Parameters
+    ----------
+    start : BinningCoords containing floats
+    stop : BinningCoords containing floats
+    num_bins : BinningCoords containing ints
+
+    Returns
+    -------
+    edges : BinningCoords containing arrays of floats
+
+    """
+    if not isinstance(start, BinningCoords):
+        start = BinningCoords(*start)
+    if not isinstance(stop, BinningCoords):
+        stop = BinningCoords(*stop)
+    if not isinstance(num_bins, BinningCoords):
+        num_bins = BinningCoords(*num_bins)
+
+    edges = BinningCoords(
+        t=np.linspace(start.t, stop.t, num_bins.t + 1),
+        r=power_axis(start=start.r, stop=stop.r, num_bins=num_bins.r, power=2),
+        theta=np.arccos(np.linspace(np.cos(start.theta),
+                                    np.cos(stop.theta),
+                                    num_bins.theta + 1)),
+        phi=np.linspace(start.phi, stop.phi, num_bins.phi + 1)
+    )
+
+    return edges
 
 
 class Events(object):
@@ -147,7 +286,8 @@ class Events(object):
         self.int_type = None
         self.load(events_fpath)
 
-    def load(self, events_fpath):
+    def load(self, events_fpath, pulse_series=DFLT_PULSE_SERIES,
+             spe_reco_name=DFLT_SPE_RECO_NAME, ml_reco_name=DFLT_ML_RECO_NAME):
         """Load events from file, populating `self`.
 
         Parameters
@@ -156,14 +296,18 @@ class Events(object):
             Path to HDF5 file
 
         """
-        print 'loading events from path "%s"' % events_fpath
+        print('loading events from path "%s"' % events_fpath)
         with h5py.File(expand(events_fpath)) as h5:
-            print h5
-            pulses = h5[PULSE_SERIES]
+            pulses = h5[pulse_series]
             pulse_events = pulses['Event']
+            strings = pulses['string']
+            print('string range: [%d, %d]' % (np.min(strings), np.max(strings)))
+            oms = pulses['om']
+            print('OM range: [%d, %d]' % (np.min(oms), np.max(oms)))
+            # NOTE: string and om indices are one less than their numbers
             self.pulses = Pulses(
-                strings=pulses['string'],
-                oms=pulses['om'],
+                strings=strings - 1,
+                oms=oms - 1,
                 times=pulses['time'],
                 charges=pulses['charge'],
             )
@@ -223,7 +367,7 @@ class Events(object):
                 color='y',
                 label='cascade'
             )
-            reco = h5[ML_RECO_NAME]
+            reco = h5[ml_reco_name]
             self.ml_recos = ParticleArray(
                 evt=reco['Event'],
                 t=reco['time'],
@@ -237,7 +381,7 @@ class Events(object):
                 color='g',
                 label='Multinest'
             )
-            reco = h5[SPE_RECO_NAME]
+            reco = h5[spe_reco_name]
             self.spe_recos = ParticleArray(
                 evt=reco['Event'],
                 t=reco['time'],
@@ -289,5 +433,3 @@ class Events(object):
             spe_reco=self.spe_recos[idx]
         )
         return event
-
-

retro/hypo_fast.py
@@ -17,7 +17,8 @@ import numpy as np
 if __name__ == '__main__' and __package__ is None:
     os.sys.path.append(dirname(dirname(abspath(__file__))))
 from retro import (BinningCoords, FTYPE, SPEED_OF_LIGHT_M_PER_NS,
-                   HYPO_PARAMS_T, hypo_to_track_params, PI_BY_TWO, TWO_PI)
+                   HYPO_PARAMS_T, hypo_to_track_params, PI_BY_TWO,
+                   TimeSpaceCoord, TRACK_M_PER_GEV, TWO_PI)
 from sparse import Sparse
 
 
@@ -91,56 +92,48 @@ class Track(object):
 
     Parameters
     ----------
-    params : TrackParams namedtuple
+    params : TrackParams
+    origin : TimeSpaceCoord
 
     """
-    def __init__(self, params):
+    def __init__(self, params, origin=(0,)*4):
         self.params = params
 
-        # Vertex position, direction, and length
-        self.t_v = params.t
-        self.x_v = params.x
-        self.y_v = params.y
-        self.z_v = params.z
-        self.theta = params.zenith
-        self.phi = params.azimuth
-        self.length = params.length
+        # Track length is derived from its energy
+        self.length = params.energy * TRACK_M_PER_GEV
 
         # Pre-calculated quantities
         self.dt = self.length / SPEED_OF_LIGHT_M_PER_NS
-        self.sinphi = np.sin(self.phi)
-        self.cosphi = np.cos(self.phi)
-        self.tanphi = np.tan(self.phi)
-        self.sintheta = np.sin(self.theta)
-        self.costheta = np.cos(self.theta)
-        self.set_origin(0, 0, 0, 0)
+        self.sinphi = np.sin(self.params.azimuth)
+        self.cosphi = np.cos(self.params.azimuth)
+        self.tanphi = np.tan(self.params.azimuth)
+        self.sintheta = np.sin(self.params.zenith)
+        self.costheta = np.cos(self.params.zenith)
+        self.origin = None
+        self.set_origin(origin)
 
-    def set_origin(self, t, x, y, z):
-        """ displace track """
-        self.t_o = t
-        self.x_o = x
-        self.y_o = y
-        self.z_o = z
+    def set_origin(self, coord):
+        """Displace track
 
-    @property
-    def t0(self):
-        """ transalted t """
-        return self.t_v - self.t_o
+        Parameters
+        ----------
+        coord : TimeSpaceCoord namedtuple
 
-    @property
-    def x0(self):
-        """ translated x """
-        return self.x_v - self.x_o
+        """
+        if coord == self.origin:
+            return
 
-    @property
-    def y0(self):
-        """ translated y """
-        return self.y_v - self.y_o
+        if not isinstance(coord, TimeSpaceCoord):
+            coord = TimeSpaceCoord(*coord)
 
-    @property
-    def z0(self):
-        """ translated z """
-        return self.z_v - self.z_o
+        self.origin = coord
+
+        # Define relative coordinates
+
+        self.t0 = self.params.t - self.origin.t
+        self.x0 = self.params.x - self.origin.x
+        self.y0 = self.params.y - self.origin.y
+        self.z0 = self.params.z - self.origin.z
 
     def point(self, t):
         """return point on track for a given time"""
@@ -176,8 +169,8 @@ class Track(object):
     def ts(self):
         """time with smallest theta"""
         if ((self.x0 == self.y0 == self.z0 == 0)
-                or self.theta == 0
-                or self.theta == np.pi):
+                or self.params.zenith == 0
+                or self.params.zenith == np.pi):
             return self.t0
 
         rho = ((- self.costheta * (self.x0*self.x0 + self.y0*self.y0)
@@ -315,12 +308,12 @@ class Hypo(object):
         self.bin_centers = None
         self.shape = tuple([])
 
-    def set_binning(self, bin_edges): #t_bin_edges, r_bin_edges, theta_bin_edges, phi_bin_edges):
+    def set_binning(self, bin_edges):
         """Set the binning of the spherical coordinates with bin_edges.
 
         Parameters
         ----------
-        bin_edges : likelihood.BinningCoords namedtuple
+        bin_edges : BinningCoords
 
         """
         self.bin_edges = bin_edges
@@ -462,30 +455,29 @@ class Hypo(object):
         return np.array(intervals, dtype=FTYPE)
 
     #@profile
-    def get_matrices(self, Dt=0, Dx=0, Dy=0, Dz=0):
+    def compute_matrices(self, hit_dom_coord):
         """Calculate the matrices for a given DOM hit, i.e.,
         DOM 3D position + hit time.
 
         Parameters
         ----------
-        Dt
-            DOM hit time (ns)
-        Dx, Dy, Dz
-            DOM position (m)
+        hit_dom_coord : TimeSpaceCoord or convertible thereto
+            Hit position in time and space.
 
         Returns
         -------
         matrix in t, r, theta, phi
-        n_photons = number of photons
-        p_theta = source direction in theta
-        p_phi = delta phi direction of source
-        p_length = correlation of photons
+        photon_counts = number of photons
+        photon_corr_theta = source direction in theta
+        photon_corr_phi = delta phi direction of source
+        photon_corr_len = correlation of photons
 
         """
-        # set the origin of the spherical coords. to the DOM hit pos.
-        self.track.set_origin(Dt, Dx, Dy, Dz)
+        # Set the origin of the spherical coordinate system to the DOM hit
+        # position
+        self.track.set_origin(hit_dom_coord)
 
-        # closest point (r inflection point)
+        # Closest point (r inflection point)
         tb = self.track.tb
         if tb >= self.track.t0 and tb < self.track.t0 + self.track.dt:
             xb, yb, zb = self.track.point(tb)
@@ -498,10 +490,10 @@ class Hypo(object):
             theta_inflection_point = self.ctheta(xs, ys, zs)
 
         # the big matrix z
-        n_photons = Sparse(shape=self.shape, default=0, dtype=FTYPE)
-        p_theta = Sparse(shape=self.shape, default=0, dtype=FTYPE)
-        p_phi = Sparse(shape=self.shape, default=0, dtype=FTYPE)
-        p_length = Sparse(shape=self.shape, default=0, dtype=FTYPE)
+        photon_counts = Sparse(shape=self.shape, default=0, dtype=FTYPE)
+        photon_corr_theta = Sparse(shape=self.shape, default=0, dtype=FTYPE)
+        photon_corr_phi = Sparse(shape=self.shape, default=0, dtype=FTYPE)
+        photon_corr_len = Sparse(shape=self.shape, default=0, dtype=FTYPE)
 
         start_t = time.time()
         t_inner_loop = 0
@@ -563,29 +555,29 @@ class Hypo(object):
                 corr_loop += end_t3 - start_t3
 
                 start_t2 = time.time()
-                n_photons = inner_loop(n_photons, k, phi_inter,
-                                       theta_inter_neg, theta_inter_pos,
-                                       r_inter_neg, r_inter_pos,
-                                       self.photons_per_meter)
+                photon_counts = inner_loop(photon_counts, k, phi_inter,
+                                           theta_inter_neg, theta_inter_pos,
+                                           r_inter_neg, r_inter_pos,
+                                           self.photons_per_meter)
                 end_t2 = time.time()
                 t_inner_loop += end_t2 - start_t2
 
         end_t = time.time()
 
         # add angles:
-        for element in n_photons:
+        for element in photon_counts:
             idx, _ = element
-            theta = self.track.theta
+            theta = self.track.params.zenith
             phi = self.bin_centers.phi[idx[-1]]
-            #delta_phi = np.abs(self.track.phi - phi)%(TWO_PI)
+            #delta_phi = np.abs(self.track.params.azimuth - phi)%(TWO_PI)
             # calculate same way as in photon propapgation (CLsim)
-            delta = np.abs(phi - self.track.phi)
+            delta = np.abs(phi - self.track.params.azimuth)
             delta_phi = delta if delta <= np.pi else TWO_PI - delta
-            #delta_phi = np.pi - np.abs((np.abs(phi - self.track.phi)  - np.pi))
-            p_theta[idx] = theta
-            p_phi[idx] = delta_phi
+            #delta_phi = np.pi - np.abs((np.abs(phi - self.track.params.azimuth)  - np.pi))
+            photon_corr_theta[idx] = theta
+            photon_corr_phi[idx] = delta_phi
             # set corr. length for tracks to 1.0, i.e. totally directed
-            p_length[idx] = 1.0
+            photon_corr_len[idx] = 1.0
 
         # add cascade as point:
         # get bin at self.track.t0, ...
@@ -604,13 +596,16 @@ class Hypo(object):
         if None not in (t_bin, r_bin, theta_bin, phi_bin):
             # Weighted average of corr length from track and cascade, while
             # assuming 0.5 for cascade right now
-            p_length[t_bin, r_bin, theta_bin, phi_bin] = np.average(
-                [p_length[t_bin, r_bin, theta_bin, phi_bin], 0.5],
-                weights=[n_photons[t_bin, r_bin, theta_bin, phi_bin], self.cascade_photons]
+            photon_corr_len[t_bin, r_bin, theta_bin, phi_bin] = np.average(
+                [photon_corr_len[t_bin, r_bin, theta_bin, phi_bin], 0.5],
+                weights=[photon_counts[t_bin, r_bin, theta_bin, phi_bin], self.cascade_photons]
             )
-            n_photons[t_bin, r_bin, theta_bin, phi_bin] += self.cascade_photons
+            photon_counts[t_bin, r_bin, theta_bin, phi_bin] += self.cascade_photons
 
-        return n_photons, p_theta, p_phi, p_length
+        self.photon_counts = photon_counts
+        self.photon_corr_theta = photon_corr_theta
+        self.photon_corr_phi = photon_corr_phi
+        self.photon_corr_len = photon_corr_len
 
     @staticmethod
     def get_bin(val, bin_edges):

retro/hypo_vector.py
@@ -12,71 +12,55 @@ import numpy as np
 
 if __name__ == '__main__' and __package__ is None:
     os.sys.path.append(dirname(dirname(abspath(__file__))))
-from retro import FTYPE, SPEED_OF_LIGHT_M_PER_NS
+from retro import (BinningCoords, CASCADE_PHOTONS_PER_GEV, FTYPE,
+                   HYPO_PARAMS_T, SPEED_OF_LIGHT_M_PER_NS, TimeSpaceCoord,
+                   TRACK_M_PER_GEV, TRACK_PHOTONS_PER_M, TWO_PI, UITYPE)
 
 
-SPEED_OF_LIGHT = SPEED_OF_LIGHT_M_PER_NS * 1e9
-TRACK_LENGTH_M_PER_GEV = 15 / 3.3
-TRACK_PHOTONS_PER_M = 2451.4544553
-CASCADE_PHOTONS_PER_GEV = 12805.3383311
-
-UITYPE = np.uint64
+NUMBA_UPDATE_ARRAYS = False
 
 N_FTYPE = numba.float32 if FTYPE is np.float32 else numba.float64
 N_ITYPE = numba.int64
 N_UITYPE = numba.uint16 if UITYPE is np.uint16 else numba.uint64
 
 
-CLASS_DTYPE_SPEC = [
-    ('t_v', N_FTYPE),
-    ('x_v', N_FTYPE),
-    ('y_v', N_FTYPE),
-    ('z_v', N_FTYPE),
-    ('theta_v', N_FTYPE),
-    ('phi_v', N_FTYPE),
-    ('track_energy', N_FTYPE),
-    ('cascade_energy', N_FTYPE),
-
-    ('time_increment', N_FTYPE),
-    ('segment_length', N_FTYPE),
-
-    ('speed_x', N_FTYPE),
-    ('speed_y', N_FTYPE),
-    ('speed_z', N_FTYPE),
-    ('track_length', N_FTYPE),
-    ('cascade_photons', N_FTYPE),
-    ('track_photons', N_FTYPE),
-    ('tot_photons', N_FTYPE),
-
-    ('n_t_bins', N_ITYPE),
-    ('n_r_bins', N_ITYPE),
-    ('n_theta_bins', N_ITYPE),
-    ('n_phi_bins', N_ITYPE),
-
-    ('t_min', N_FTYPE),
-    ('t_max', N_FTYPE),
-    ('r_min', N_FTYPE),
-    ('r_max', N_FTYPE),
-    ('t_scaling_factor', N_FTYPE),
-    ('r_scaling_factor', N_FTYPE),
-    ('theta_scaling_factor', N_FTYPE),
-    ('phi_scaling_factor', N_FTYPE),
-    ('phi_bin_width', N_FTYPE),
-
-    ('t', N_FTYPE),
-    ('x', N_FTYPE),
-    ('y', N_FTYPE),
-    ('z', N_FTYPE),
-
-    ('t_start', N_FTYPE),
-    ('t_stop', N_FTYPE),
-    ('number_of_increments', N_ITYPE),
-    ('recreate_arrays', numba.boolean),
-
-    ('variables_array', N_FTYPE[:, :]),
-    ('indices_array', N_UITYPE[:, :]),
-    ('values_array', N_FTYPE[:, :]),
-]
+#CLASS_DTYPE_SPEC = [
+#    ('t_v', N_FTYPE),
+#    ('x_v', N_FTYPE),
+#    ('y_v', N_FTYPE),
+#    ('z_v', N_FTYPE),
+#
+#    ('time_increment', N_FTYPE),
+#    ('segment_length', N_FTYPE),
+#
+#    ('speed_x', N_FTYPE),
+#    ('speed_y', N_FTYPE),
+#    ('speed_z', N_FTYPE),
+#    ('track_length', N_FTYPE),
+#    ('cascade_photons', N_FTYPE),
+#    ('track_photons', N_FTYPE),
+#    ('tot_photons', N_FTYPE),
+#
+#    ('t_scaling_factor', N_FTYPE),
+#    ('r_scaling_factor', N_FTYPE),
+#    ('theta_scaling_factor', N_FTYPE),
+#    ('track_azimuth_scaling_factor', N_FTYPE),
+#    ('phi_bin_width', N_FTYPE),
+#
+#    ('t', N_FTYPE),
+#    ('x', N_FTYPE),
+#    ('y', N_FTYPE),
+#    ('z', N_FTYPE),
+#
+#    ('t_start', N_FTYPE),
+#    ('t_stop', N_FTYPE),
+#    ('number_of_increments', N_ITYPE),
+#    ('recreate_arrays', numba.boolean),
+#
+#    ('variables_array', N_FTYPE[:, :]),
+#    ('indices_array', N_UITYPE[:, :]),
+#    ('values_array', N_FTYPE[:, :]),
+#]
 
 # Define indices for accessing rows of `variables_array`
 T_VAR_IX = 0
@@ -84,7 +68,7 @@ X_VAR_IX = 1
 Y_VAR_IX = 2
 Z_VAR_IX = 3
 R_VAR_IX = 4
-THETA_VAR_IX = 5
+TRCK_ZEN_VAR_IX = 5
 PHI_VAR_IX = 6
 
 # Define indices for accessing rows of `indices_array`
@@ -95,8 +79,55 @@ PHI_IDX_IX = 3
 
 # Define indices for accessing rows of `values_array`
 PHOT_VAL_IX = 0
-TRCK_THETA_VAL_IX = 1
-DPHI_VAL_IX = 2
+TRCK_ZEN_VAL_IX = 1
+DTRCK_AZ_VAL_IX = 2
+
+
+#@numba.jit(nopython=True, fastmath=True, parallel=True, cache=True)
+def update_arrays(indices_array, values_array,
+                  number_of_increments, t_array_init, t, x, y, z, speed_x,
+                  speed_y, speed_z, t_scaling_factor, r_scaling_factor,
+                  theta_scaling_factor, track_azimuth_scaling_factor, segment_length,
+                  cascade_photons, track_zenith, track_azimuth, phi_bin_width):
+    for seg_idx in range(number_of_increments):
+        var_t = t_array_init[seg_idx]
+        relative_time = var_t - t
+        var_x = x + speed_x * relative_time
+        var_y = y + speed_y * relative_time
+        var_z = z + speed_z * relative_time
+        var_r = math.sqrt(var_x*var_x + var_y*var_y + var_z*var_z)
+        var_theta = var_z / var_r
+        var_phi = math.atan2(var_y, var_x) % TWO_PI
+
+        #variables_array[T_VAR_IX, seg_idx] = var_t
+        #variables_array[X_VAR_IX, seg_idx] = var_x
+        #variables_array[Y_VAR_IX, seg_idx] = var_y
+        #variables_array[Z_VAR_IX, seg_idx] = var_z
+        #variables_array[R_VAR_IX, seg_idx] = var_r
+        #variables_array[TRCK_ZEN_VAR_IX, seg_idx] = var_theta
+        #variables_array[PHI_VAR_IX, seg_idx] = var_phi
+
+        indices_array[T_IDX_IX, seg_idx] = var_t * t_scaling_factor
+        indices_array[R_IDX_IX, seg_idx] = math.sqrt(var_r * r_scaling_factor)
+        indices_array[THETA_IDX_IX, seg_idx] = (1 - var_theta) * theta_scaling_factor
+        ind_phi = var_phi * track_azimuth_scaling_factor
+        indices_array[PHI_IDX_IX, seg_idx] = ind_phi
+
+        # Add track photons
+        values_array[PHOT_VAL_IX, seg_idx] = segment_length * TRACK_PHOTONS_PER_M
+
+        # TODO: should this be += to include both track and cascade photons at
+        # 0? Or are all track photons accounted for at the "bin center" which
+        # would be the first increment after 0?
+
+        # Add track theta values
+        values_array[TRCK_ZEN_VAL_IX, seg_idx] = track_zenith
+
+        # Add delta phi values
+        values_array[DTRCK_AZ_VAL_IX, seg_idx] = abs(track_azimuth - (ind_phi * phi_bin_width + (phi_bin_width / 2)))
+
+    # Set cascade photons in 0th sample location
+    values_array[PHOT_VAL_IX, 0] = cascade_photons
 
 
 # TODO: jitclass makes this go insanely slow. What's the deal with that?
@@ -109,125 +140,107 @@ class SegmentedHypo(object):
 
     Parameters
     ----------
-    t_v
-        time (ns)
+    params : HYPO_PARAMS_T
 
-    x_v, y_v, z_v
-        vertex position (m)
+    track_e_scale
+        ?
 
-    theta_v
-        zenith (rad)
-
-    phi_v
-        azimuth (rad)
-
-    track_energy
-        track energy (GeV)
-
-    cascade_energy
-        cascade energy (GeV)
+    cascade_e_scale
+        ?
 
     time_increment
         If using constant time increments, length of time between photon
         dumps (ns)
 
     """
-    def __init__(self, t_v, x_v, y_v, z_v, theta_v, phi_v, track_energy,
-                 cascade_energy, time_increment=1):
-        # Assign vertex
-        self.t = t_v * 1e-9
-        self.x = x_v
-        self.y = y_v
-        self.z = z_v
-        self.theta_v = theta_v
-        self.phi_v = phi_v
+    def __init__(self, params, cascade_e_scale=1, track_e_scale=1,
+                 time_increment=1):
+        if not isinstance(params, HYPO_PARAMS_T):
+            params = HYPO_PARAMS_T(*params)
+        self.params = params
 
-        # Declare constants
-        self.time_increment = time_increment * 1e-9
-        self.segment_length = self.time_increment * SPEED_OF_LIGHT
+        # Declare "constants"
+        self.time_increment = time_increment
+        self.segment_length = self.time_increment * SPEED_OF_LIGHT_M_PER_NS
 
-        # Calculate frequently used values
-        sin_theta_v = math.sin(self.theta_v)
-        cos_theta_v = math.cos(self.theta_v)
-        sin_phi_v = math.sin(self.phi_v)
-        cos_phi_v = math.cos(self.phi_v)
-        self.speed_x = SPEED_OF_LIGHT * sin_theta_v * cos_phi_v
-        self.speed_y = SPEED_OF_LIGHT * sin_theta_v * sin_phi_v
-        self.speed_z = SPEED_OF_LIGHT * cos_theta_v
+        c_sin_zen = SPEED_OF_LIGHT_M_PER_NS * math.sin(self.params.track_zenith)
+        self.speed_x = c_sin_zen * math.cos(self.params.track_azimuth)
+        self.speed_y = c_sin_zen * math.sin(self.params.track_azimuth)
+        self.speed_z = SPEED_OF_LIGHT_M_PER_NS * math.cos(self.params.track_zenith)
 
-        # Cconvert track energy to length
-        self.track_length = track_energy * TRACK_LENGTH_M_PER_GEV
+        self.track_length = params.track_energy * TRACK_M_PER_GEV
 
-        # Precalculate (nphotons.py) to avoid icetray
-        self.cascade_photons = cascade_energy * CASCADE_PHOTONS_PER_GEV
+        self.cascade_photons = params.cascade_energy * CASCADE_PHOTONS_PER_GEV
         self.track_photons = self.track_length * TRACK_PHOTONS_PER_M
         self.tot_photons = self.cascade_photons + self.track_photons
 
-        # Defaults
+        # Default values
         self.number_of_increments = 0
         self.recreate_arrays = True
+        self.dom_coord = None
 
-    def set_binning(self, n_t_bins, n_r_bins, n_theta_bins, n_phi_bins, t_max,
-                    r_max, t_min, r_min):
+    def set_binning(self, start, stop, num_bins):
         """Define binnings of spherical coordinates assuming: linear binning in
         time, quadratic binning in radius, linear binning in cos(theta), and
         linear binning in phi.
 
         Parameters
         ----------
-        n_t_bins, n_r_bins, n_theta_bins, n_phi_bins : int
-            Number of bins (note this is _not_ bin edges) in each dimension.
+        start : BinningCoords namedtuple containing floats
+            Lower-most bin edge in each dimension.
 
-        t_max : float
-            max time (ns)
+        stop : BinningCoords namedtuple containing floats
+            Upper-most bin edge in each dimension.
 
-        r_max : float
-            max radius (m)
-
-        t_min : float
-            min time (ns)
-
-        r_min : float
-            min radius (m)
+        num_bins : BinningCoords namedtuple containing ints
+            Number of bins in each dimension (note there will be ``num_bins + 1``
+            bin edges).
 
         """
-        self.n_t_bins = n_t_bins
-        self.n_r_bins = n_r_bins
-        self.n_theta_bins = n_theta_bins
-        self.n_phi_bins = n_phi_bins
-        self.t_min = t_min * 1e-9
-        self.t_max = t_max * 1e-9
-        self.r_min = r_min
-        self.r_max = r_max
-        self.t_scaling_factor = self.n_t_bins / (self.t_max - self.t_min)
-        self.r_scaling_factor = self.n_r_bins * self.n_r_bins / self.r_max
-        self.theta_scaling_factor = self.n_theta_bins / 2.
-        self.phi_scaling_factor = self.n_phi_bins / np.pi / 2.
-        self.phi_bin_width = 2. * np.pi / self.n_phi_bins
+        if not isinstance(num_bins, BinningCoords):
+            num_bins = BinningCoords(*num_bins)
+        if not isinstance(start, BinningCoords):
+            start = BinningCoords(*start)
+        if not isinstance(stop, BinningCoords):
+            stop = BinningCoords(*stop)
+
+        self.num_bins = num_bins
+        self.bin_min = start
+        self.bin_max = stop
+
+        self.t_scaling_factor = self.num_bins.t / (self.bin_max.t - self.bin_min.t)
+        self.r_scaling_factor = self.num_bins.r * self.num_bins.r / self.bin_max.r
+        self.theta_scaling_factor = self.num_bins.theta / 2
+        self.track_azimuth_scaling_factor = self.num_bins.phi / TWO_PI
+        self.phi_bin_width = TWO_PI / self.num_bins.phi
 
     #@profile
-    def set_dom_location(self, t_dom=0, x_dom=0, y_dom=0, z_dom=0):
+    def set_dom_location(self, coord):
         """Change the track vertex to be relative to a DOM at a given position.
 
         Parameters
         ----------
-        t_dom
-            time (ns)
-
-        x_dom, y_dom, z_dom
-            position of the dom (m)
+        coord : TimeSpaceCoord or convertible thereto
 
         """
-        self.t = self.t - t_dom * 1e-9
-        self.x = self.x - x_dom
-        self.y = self.y - y_dom
-        self.z = self.z - z_dom
+        if coord == self.dom_coord:
+            return
+
+        if not isinstance(coord, TimeSpaceCoord):
+            coord = TimeSpaceCoord(*coord)
+
+        self.dom_coord = coord
+
+        self.t = self.params.t - coord.t
+        self.x = self.params.x - coord.x
+        self.y = self.params.y - coord.y
+        self.z = self.params.z - coord.z
 
         orig_number_of_incr = self.number_of_increments
 
         # Create initial time array, using the midpoints of each time increment
-        incr_by_2 = self.time_increment / 2
-        self.t_array_init = np.arange(self.t - incr_by_2, min(self.t_max, self.track_length / SPEED_OF_LIGHT + self.t) - incr_by_2, self.time_increment, FTYPE)
+        half_incr = self.time_increment / 2
+        self.t_array_init = np.arange(self.t - half_incr, min(self.bin_max.t, self.track_length / SPEED_OF_LIGHT_M_PER_NS + self.t) - half_incr, self.time_increment, FTYPE)
         self.t_array_init[0] = self.t
 
         # Set the number of time increments in the track
@@ -238,39 +251,78 @@ class SegmentedHypo(object):
             self.recreate_arrays = True
 
     #@profile
-    def vector_photon_matrix(self):
+    def compute_matrices(self, hit_dom_coord):
         """Use a single time array to simultaneously calculate all of the
         positions along the track, using information from __init__.
 
         """
+        self.set_dom_location(coord=hit_dom_coord)
+
         # Create array with variables
         if self.recreate_arrays:
-            self.variables_array = np.empty((8, self.number_of_increments),
-                                            FTYPE)
+            #self.variables_array = np.empty(
+            #    (8, self.number_of_increments),
+            #    FTYPE
+            #)
+            self.indices_array = np.empty(
+                (4, self.number_of_increments),
+                UITYPE
+            )
+            self.values_array = np.empty(
+                (3, self.number_of_increments),
+                FTYPE
+            )
+            self.recreate_arrays = False
 
-        self.variables_array[T_VAR_IX, :] = self.t_array_init
+        if NUMBA_UPDATE_ARRAYS:
+            update_arrays(self.indices_array,
+                          self.values_array,
+                          self.number_of_increments,
+                          self.t_array_init,
+                          self.t,
+                          self.x,
+                          self.y,
+                          self.z,
+                          self.speed_x,
+                          self.speed_y,
+                          self.speed_z,
+                          self.t_scaling_factor,
+                          self.r_scaling_factor,
+                          self.theta_scaling_factor,
+                          self.track_azimuth_scaling_factor,
+                          self.segment_length,
+                          self.cascade_photons,
+                          self.params.track_zenith,
+                          self.params.track_azimuth,
+                          self.phi_bin_width)
+            return
 
-        relative_time = self.variables_array[T_VAR_IX, :] - self.t
+        #self.variables_array[T_VAR_IX, :] = self.t_array_init
+        #relative_time = self.variables_array[T_VAR_IX, :] - self.t
+        #self.variables_array[X_VAR_IX, :] = self.x + self.speed_x * relative_time
+        #self.variables_array[Y_VAR_IX, :] = self.y + self.speed_y * relative_time
+        #self.variables_array[Z_VAR_IX, :] = self.z + self.speed_z * relative_time
+        #self.variables_array[R_VAR_IX, :] = np.sqrt(np.square(self.variables_array[X_VAR_IX, :]) + np.square(self.variables_array[Y_VAR_IX, :]) + np.square(self.variables_array[Z_VAR_IX, :]))
+        #self.variables_array[TRCK_ZEN_VAR_IX, :] = self.variables_array[Z_VAR_IX, :] / self.variables_array[R_VAR_IX, :]
+        #self.variables_array[PHI_VAR_IX, :] = np.arctan2(self.variables_array[Y_VAR_IX, :], self.variables_array[X_VAR_IX, :]) % TWO_PI
 
-        self.variables_array[X_VAR_IX, :] = self.x + self.speed_x * relative_time
-        self.variables_array[Y_VAR_IX, :] = self.y + self.speed_y * relative_time
-        self.variables_array[Z_VAR_IX, :] = self.z + self.speed_z * relative_time
-        self.variables_array[R_VAR_IX, :] = np.sqrt(np.square(self.variables_array[X_VAR_IX, :]) + np.square(self.variables_array[Y_VAR_IX, :]) + np.square(self.variables_array[Z_VAR_IX, :]))
-        self.variables_array[THETA_VAR_IX, :] = self.variables_array[Z_VAR_IX, :] / self.variables_array[R_VAR_IX, :]
-        self.variables_array[PHI_VAR_IX, :] = np.arctan2(self.variables_array[Y_VAR_IX, :], self.variables_array[X_VAR_IX, :]) % (2 * np.pi)
+        #self.indices_array[T_IDX_IX, :] = self.variables_array[T_VAR_IX, :] * self.t_scaling_factor
+        #self.indices_array[R_IDX_IX, :] = np.sqrt(self.variables_array[R_VAR_IX, :] * self.r_scaling_factor)
+        #self.indices_array[THETA_IDX_IX, :] = (1 - self.variables_array[TRCK_ZEN_VAR_IX, :]) * self.theta_scaling_factor
+        #self.indices_array[PHI_IDX_IX, :] = self.variables_array[PHI_VAR_IX, :] * self.track_azimuth_scaling_factor
 
-        # Create array with indices
-        if self.recreate_arrays:
-            self.indices_array = np.empty((4, self.number_of_increments), UITYPE)
+        relative_time = self.t_array_init - self.t
+        var_x = self.x + self.speed_x * relative_time
+        var_y = self.y + self.speed_y * relative_time
+        var_z = self.z + self.speed_z * relative_time
+        var_r = np.sqrt(np.square(var_x) + np.square(var_y) + np.square(var_z))
+        var_theta = var_z / var_r
+        var_phi = np.arctan2(var_y, var_x) % TWO_PI
 
-        self.indices_array[T_IDX_IX, :] = self.variables_array[T_VAR_IX, :] * self.t_scaling_factor
-        self.indices_array[R_IDX_IX, :] = np.sqrt(self.variables_array[R_VAR_IX, :] * self.r_scaling_factor)
-        self.indices_array[THETA_IDX_IX, :] = (1 - self.variables_array[THETA_VAR_IX, :]) * self.theta_scaling_factor
-        self.indices_array[PHI_IDX_IX, :] = self.variables_array[PHI_VAR_IX, :] * self.phi_scaling_factor
-
-        # Create array to store values for each index
-        if self.recreate_arrays:
-            self.values_array = np.empty((3, self.number_of_increments), FTYPE)
+        self.indices_array[T_IDX_IX, :] = relative_time * self.t_scaling_factor
+        self.indices_array[R_IDX_IX, :] = np.sqrt(var_r * self.r_scaling_factor)
+        self.indices_array[THETA_IDX_IX, :] = (1 - var_theta) * self.theta_scaling_factor
+        self.indices_array[PHI_IDX_IX, :] = var_phi * self.track_azimuth_scaling_factor
 
         # Add track photons
         self.values_array[PHOT_VAL_IX, :] = self.segment_length * TRACK_PHOTONS_PER_M
@@ -279,16 +331,15 @@ class SegmentedHypo(object):
         # 0? Or are all track photons accounted for at the "bin center" which
         # would be the first increment after 0?
 
-        # Add cascade photons
+        # Set cascade photons in 0th sample location
         self.values_array[PHOT_VAL_IX, 0] = self.cascade_photons
 
         # Add track theta values
-        self.values_array[TRCK_THETA_VAL_IX, :] = self.theta_v
+        self.values_array[TRCK_ZEN_VAL_IX, :] = self.params.track_zenith
 
         # Add delta phi values
-        self.values_array[DPHI_VAL_IX, :] = np.abs(
-            self.phi_v - (self.indices_array[PHI_IDX_IX, :]
-                          * self.phi_bin_width + (self.phi_bin_width / 2))
+        self.values_array[DTRCK_AZ_VAL_IX, :] = np.abs(
+            self.params.track_azimuth
+            - (self.indices_array[PHI_IDX_IX, :] * self.phi_bin_width
+               + (self.phi_bin_width / 2))
         )
-
-        self.recreate_arrays = False

retro/likelihood.py
@@ -17,7 +17,7 @@ from collections import OrderedDict
 from copy import deepcopy
 from itertools import izip, product
 import os
-from os.path import abspath, dirname, expanduser, expandvars, isfile, join
+from os.path import abspath, dirname, isfile, join
 
 import h5py
 import matplotlib as mpl
@@ -31,8 +31,9 @@ from scipy.special import gammaln
 
 if __name__ == '__main__' and __package__ is None:
     os.sys.path.append(dirname(dirname(abspath(__file__))))
-from retro import (BinningCoords, event_to_hypo_params, Event, Events, FTYPE,
-                   HypoParams10D, HYPO_PARAMS_T, PhotonInfo, Pulses)
+from retro import (BinningCoords, event_to_hypo_params, Event, Events, expand,
+                   FTYPE, HypoParams10D, HYPO_PARAMS_T, PhotonInfo, Pulses,
+                   TimeSpaceCoord)
 import hypo_fast
 import hypo_vector
 from particles import ParticleArray
@@ -40,18 +41,15 @@ from particles import ParticleArray
 
 IC_TABLE_FPATH_PROTO = (
     '/data/icecube/retro_tables/full1000'
-    '/retro_nevts1000_IC_DOM%i_r_cz_t_angles.fits'
+    '/retro_nevts1000_IC_DOM{:d}_r_cz_t_angles.fits'
 )
 
 DC_TABLE_FPATH_PROTO = (
     '/data/icecube/retro_tables/full1000'
-    '/retro_nevts1000_DC_DOM%i_r_cz_t_angles.fits'
+    '/retro_nevts1000_DC_DOM{:d}_r_cz_t_angles.fits'
 )
 
-PULSE_SERIES = 'SRTInIcePulses'
-ML_RECO_NAME = 'IC86_Dunkman_L6_PegLeg_MultiNest8D_NumuCC'
-SPE_RECO_NAME = 'SPEFit2'
-DETECTOR_GEOM_FILE = join(dirname(__file__), 'data', 'geo_array.npy')
+DETECTOR_GEOM_FILE = join(dirname(abspath(__file__)), 'data', 'geo_array.npy')
 DFLT_EVENTS_FPATH = (
     '/fastio/icecube/deepcore/data/MSU_sample/level5pt/numu/14600'
     '/icetray_hdf5/Level5pt_IC86.2013_genie_numu.014600.000000.hdf5'
@@ -98,8 +96,7 @@ MIN_USE_RELATIVE_BOUNDS = True
 SCAN_USE_RELATIVE_BOUNDS = True
 """Whether scanning is to use relative bounds (where defined)"""
 
-MIN_DIMS = [] #('t', 'x', 'y', 'z', 'track_zenith', 'track_azimuth',
-#                'track_energy')
+MIN_DIMS = [] #('t x y z track_zenith track_azimuth track_energy'.split())
 """Which dimensions to plug into minimizer (dims not fixed to truth)"""
 
 SCAN_DIM_SETS = (
@@ -110,29 +107,34 @@ SCAN_DIM_SETS = (
 """Which dimensions to scan. Tuples specify 2+ dimension scans"""
 
 
-def expand(p):
-    """Expand path"""
-    return expanduser(expandvars(p))
-
 #@profile
-def fill_photon_info(fpath, dom, scale=1, photon_info=None):
+def fill_photon_info(fpath, dom_depth_index, scale=1, photon_info=None):
     """Fill photon info namedtuple-of-dictionaries from FITS file.
 
     Parameters
     ----------
     fpath : string
-    dom : int
+        Path to FITS file corresponding to the passed ``dom_depth_index``.
+
+    dom_depth_index : int
         Depth index (e.g. from 0 to 59)
-    norm : float
-    dom_eff : float
-    photon_info : None or PhotonInfo
+
+    scale : float
+        Scaling factor to apply to the photon counts from the table, e.g. for
+        DOM efficiency.
+
+    photon_info : None or PhotonInfo namedtuple of dicts
         If None, creates a new PhotonInfo namedtuple with empty dicts to fill.
         If one is provided, the existing component dictionaries are updated.
 
     Returns
     -------
-    photon_info : PhotonInfo namedtuple
+    photon_info : PhotonInfo namedtuple of dicts
+        Tuple fields are 'count', 'theta', 'phi', and 'length'. Each dict is
+        keyed by `dom_depth_index` and values are the arrays loaded from the FITS file.
+
     bin_edges : BinningCoords namedtuple
+        Each element of the tuple is an array of bin edges.
 
     """
     # pylint: disable=no-member
@@ -140,10 +142,14 @@ def fill_photon_info(fpath, dom, scale=1, photon_info=None):
         photon_info = PhotonInfo(*([{}]*len(PhotonInfo._fields)))
 
     with pyfits.open(expand(fpath)) as table:
-        photon_info.count[dom] = table[0].data * scale
-        photon_info.theta[dom] = table[1].data
-        photon_info.phi[dom] = table[2].data
-        photon_info.length[dom] = table[3].data
+        if scale == 1:
+            photon_info.count[dom_depth_index] = table[0].data
+        else:
+            photon_info.count[dom_depth_index] = table[0].data * scale
+
+        photon_info.theta[dom_depth_index] = table[1].data
+        photon_info.phi[dom_depth_index] = table[2].data
+        photon_info.length[dom_depth_index] = table[3].data
 
         # Note that we invert (reverse and multiply by -1) time edges
         bin_edges = BinningCoords(t=-table[4].data[::-1], r=table[5].data,
@@ -153,15 +159,21 @@ def fill_photon_info(fpath, dom, scale=1, photon_info=None):
 
 
 #@profile
-def get_llh(hypo, event, detector_geometry, ic_photon_info, dc_photon_info):
+def get_llh(hypo, event, detector_geometry, ic_photon_info, dc_photon_info,
+            detailed_info_list=None):
     """Get log likelihood.
 
     Parameters
     ----------
     hypo : HYPO_T
-    event : Event namedtuple
-    ic_photon_info : dict
-    dc_photon_info : dict
+    event : retro.Event namedtuple or convertible thereto
+    detector_geometry : numpy.ndarray
+    ic_photon_info : retro.PhotonInfo namedtuple or convertible thereto
+    dc_photon_info : retro.PhotonInfo namedtuple or convertible thereto
+    detailed_info_list : None or appendable sequence
+        If a list is provided, it is appended with a dict containing detailed
+        info from the calculation useful, e.g., for debugging. If ``None`` is
+        passed, no detailed info is made available.
 
     Returns
     -------
@@ -169,48 +181,51 @@ def get_llh(hypo, event, detector_geometry, ic_photon_info, dc_photon_info):
 
     """
     llh = 0
-    n_noise = 0
+    noise_counts = 0
 
     # Loop over pulses (aka hits)
     for string, om, pulse_time, pulse_charge in izip(*event.pulses):
         x, y, z = detector_geometry[string, om]
 
+        hit_dom_coord = TimeSpaceCoord(t=pulse_time, x=x, y=y, z=z)
+
         # Get the photon expectations of the hypothesis in the DOM-hit
         # coordinates
-        n_phot, photon_theta, photon_phi, photon_length = hypo.get_matrices(
-            Dt=pulse_time, Dx=x, Dy=y, Dz=z
-        )
+        hypo.compute_matrices(hit_dom_coord=hit_dom_coord)
 
         # Get the retro table for the pulse
-        retro_idx = om - 1
-        if string < 79:
-            # these are ordinary icecube strings
-            n_phot_map = ic_photon_info.count[retro_idx]
-            photon_theta_map = ic_photon_info.theta[retro_idx]
-            photon_phi_map = ic_photon_info.phi[retro_idx]
-            photon_length_map = ic_photon_info.length[retro_idx]
+
+        # String indices 0-78 (numbers 1-79) are ordinary IceCube strings
+        if 0 <= string <= 78:
+            photon_counts_map = ic_photon_info.count[om]
+            photon_corr_theta_map = ic_photon_info.theta[om]
+            photon_corr_phi_map = ic_photon_info.phi[om]
+            photon_corr_len_map = ic_photon_info.length[om]
+        # String indices 79-85 (numbers 80-86) are ordinary DeepCore strings
+        elif 79 <= string <= 85:
+            photon_counts_map = dc_photon_info.count[om]
+            photon_corr_theta_map = dc_photon_info.theta[om]
+            photon_corr_phi_map = dc_photon_info.phi[om]
+            photon_corr_len_map = dc_photon_info.length[om]
         else:
-            # these are deepocre strings
-            n_phot_map = dc_photon_info.count[retro_idx]
-            photon_theta_map = dc_photon_info.theta[retro_idx]
-            photon_phi_map = dc_photon_info.phi[retro_idx]
-            photon_length_map = dc_photon_info.length[retro_idx]
+            raise ValueError('Unhandled string index %d (number %d)'
+                             % (string, string + 1))
 
         # Get max llh between z_matrix and gamma_map
         # noise probability?
         expected_charge = 0
-        for photon_idx, hypo_count in n_phot:
+        for photon_idx, hypo_count in hypo.photon_counts:
             # These two agles need to be inverted, because we're
             # backpropagating but want to match to forward-propagating photons
-            hypo_theta = np.pi - photon_theta[photon_idx]
-            hypo_phi = np.pi - photon_phi[photon_idx]
-            #hypo_legth = photon_length[photon_idx]
+            hypo_theta = np.pi - hypo.photon_corr_theta[photon_idx]
+            hypo_phi = np.pi - hypo.photon_corr_phi[photon_idx]
+            #hypo_legth = hypo.photon_corr_length[photon_idx]
 
             # Get map
-            map_count = n_phot_map[photon_idx[0:3]]
-            map_theta = photon_theta_map[photon_idx[0:3]]
-            map_phi = photon_phi_map[photon_idx[0:3]]
-            map_length = photon_length_map[photon_idx[0:3]]
+            map_count = photon_counts_map[photon_idx[0:3]]
+            map_theta = photon_corr_theta_map[photon_idx[0:3]]
+            map_phi = photon_corr_phi_map[photon_idx[0:3]]
+            map_length = photon_corr_len_map[photon_idx[0:3]]
 
             # Assume source is totally directed at 0.73 rad (cherenkov angle)
 
@@ -248,13 +263,16 @@ def get_llh(hypo, event, detector_geometry, ic_photon_info, dc_photon_info):
             expected_charge += total_charge * accept_dir
 
         if expected_charge < NOISE_CHARGE:
-            n_noise += 1
+            noise_counts += 1
 
         expected_charge = max(NOISE_CHARGE, expected_charge)
         pulse_llh = -(pulse_charge * np.log(expected_charge)
                       - expected_charge
                       - gammaln(pulse_charge + 1))
         llh += pulse_llh
+
+    if detailed_info_list is not None:
+        detailed_info_list.append(dict(noise_counts=noise_counts))
 
     return llh
 
@@ -396,13 +414,13 @@ def main(events_fpath, start_index=None, stop_index=None):
 
     # Read in the actual tables
     ref_bin_edges = None
-    for dom in range(60):
+    for dom_depth_index in range(60):
         # IceCube (non-DeepCore) tables
-        fpath = IC_TABLE_FPATH_PROTO % dom
+        fpath = IC_TABLE_FPATH_PROTO.format(dom_depth_index)
         if isfile(fpath):
             ic_photon_info, bin_edges = fill_photon_info(
                 fpath=fpath,
-                dom=dom,
+                dom_depth_index=dom_depth_index,
                 scale=norm * dom_eff_ic,
                 photon_info=ic_photon_info
             )
@@ -412,14 +430,15 @@ def main(events_fpath, start_index=None, stop_index=None):
                 for test, ref in zip(bin_edges, ref_bin_edges):
                     assert np.array_equal(test, ref)
         else:
-            print 'No table for IC DOM depth index %i found at path "%s"' % (dom, fpath)
+            print ('No table for IC DOM depth index %i found at path "%s"'
+                   % (dom_depth_index, fpath))
 
         # DeepCore tables
-        fpath = DC_TABLE_FPATH_PROTO % dom
+        fpath = DC_TABLE_FPATH_PROTO.format(dom_depth_index)
         if isfile(fpath):
             dc_photon_info, bin_edges = fill_photon_info(
                 fpath=fpath,
-                dom=dom,
+                dom_depth_index=dom_depth_index,
                 scale=norm * dom_eff_dc,
                 photon_info=dc_photon_info
             )
@@ -429,7 +448,8 @@ def main(events_fpath, start_index=None, stop_index=None):
                 for test, ref in zip(bin_edges, ref_bin_edges):
                     assert np.array_equal(test, ref)
         else:
-            print 'No table for IC DOM depth index %i found at path "%s"' % (dom, fpath)
+            print ('No table for IC DOM depth index %i found at path "%s"'
+                   % (dom_depth_index, fpath))
 
     bin_edges = BinningCoords(t=bin_edges.t, r=bin_edges.r,
                               theta=bin_edges.theta,

retro/plot.py
@@ -3,7 +3,7 @@
 # pylint: disable=print-statement, wrong-import-position
 
 
-from __future__ import absolute_import, division
+from __future__ import absolute_import, division, print_function
 
 import math
 import os
@@ -19,78 +19,73 @@ import numpy as np
 
 if __name__ == '__main__' and __package__ is None:
     os.sys.path.append(dirname(dirname(abspath(__file__))))
-from retro import BinningCoords, FTYPE, HypoParams8D
-from power_axis import PowerAxis
+from retro import (BinningCoords, binspec_to_edges, FTYPE, HypoParams8D,
+                   TimeSpaceCoord)
 from hypo_vector import SegmentedHypo
 from hypo_fast import Hypo
 
 #@profile
 def main():
-    # Same as CLsim
-    n_t_bins = 50
-    n_r_bins = 20
-    n_theta_bins = 50
-    n_phi_bins = 36
+    # Binning defined as same as that used for CLsim
+    bin_start = BinningCoords(t=0, r=0, theta=0, phi=0)
+    bin_stop = BinningCoords(t=500, r=200, theta=np.pi, phi=2*np.pi)
+    num_bins = BinningCoords(t=50, r=20, theta=50, phi=36)
+    bin_edges = binspec_to_edges(start=bin_start, stop=bin_stop,
+                                 num_bins=num_bins)
 
-    t_min, t_max = 0, 500
-    r_min, r_max = 0, 200
-
-    bin_edges = BinningCoords(
-        t=np.linspace(t_min, t_max, n_t_bins + 1),
-        r=PowerAxis(r_min, r_max, n_r_bins, 2),
-        theta=np.arccos(np.linspace(-1, 1, n_theta_bins + 1))[::-1],
-        phi=np.linspace(0, 2*np.pi, n_phi_bins + 1)
-    )
-    binning_shape = tuple(len(edges) - 1 for edges in bin_edges)
-
+    # An arbitrary hypothesis for testing
     hypo_params = HypoParams8D(t=65, x=1, y=10, z=-50, track_zenith=1.08,
                                track_azimuth=0.96, track_energy=20,
                                cascade_energy=25)
 
-    t0 = time.time()
-    my_hypo = Hypo(hypo_params)
-    my_hypo.set_binning(bin_edges)
-    hits, n_t, n_p, n_l = my_hypo.get_matrices(50, 0, 10, 0)
-    print ('took %5.2f ms to calculate philipp z matrix'
-           % ((time.time() - t0)*1000))
+    # An arbitrary hit coordinate for testing
+    hit_dom_coord = TimeSpaceCoord(t=50, x=0, y=10, z=0)
 
-    z = np.zeros(binning_shape)
-    for hit in hits:
-        #print hit
+    t0 = time.time()
+    hypo_ana_fast = Hypo(hypo_params)
+    hypo_ana_fast.set_binning(bin_edges)
+    hypo_ana_fast.compute_matrices(hit_dom_coord=hit_dom_coord)
+    print('took %5.2f ms to calculate philipp z matrix'
+          % ((time.time() - t0)*1000))
+
+    z = np.zeros(num_bins)
+    for hit in hypo_ana_fast.photon_counts:
         idx, count = hit
         z[idx] = count
-    print ('total number of photons in philipp z matrix = %i (%0.2f %%)'
-           % (z.sum(), z.sum()/my_hypo.tot_photons*100))
-    print ''
+    print('total number of photons in philipp z matrix = %i (%0.2f %%)'
+          % (z.sum(), z.sum() / hypo_ana_fast.tot_photons * 100))
+
+    print('')
 
     # kevin array
     t0 = time.time()
-    kevin_hypo = SegmentedHypo(*hypo_params, time_increment=1)
-    # NOTE: jitclass w/ SegmentedHypo doesn't allow for kwargs to methods. (?)
-    kevin_hypo.set_binning(n_t_bins, n_r_bins, n_theta_bins, n_phi_bins, t_max, r_max, r_min, t_min)
-    kevin_hypo.set_dom_location(50, 0, 10, 0)
-    kevin_hypo.vector_photon_matrix()
-    print ('took %5.2f ms to calculate kevin z matrix'
-           % ((time.time() - t0)*1000))
-    z_indices = kevin_hypo.indices_array
-    z_values = kevin_hypo.values_array
-    z_matrix = np.zeros(binning_shape, dtype=FTYPE)
+    hypo_approx = SegmentedHypo(params=hypo_params, time_increment=1)
+    hypo_approx.set_binning(start=bin_start, stop=bin_stop, num_bins=num_bins)
+    hypo_approx.compute_matrices(hit_dom_coord)
+    print('took %5.2f ms to calculate kevin z matrix'
+          % ((time.time() - t0)*1000))
 
-    for col in xrange(kevin_hypo.number_of_increments):
+    z_indices = hypo_approx.indices_array
+    z_values = hypo_approx.values_array
+    z_matrix = np.zeros(num_bins, dtype=FTYPE)
+
+    for col in xrange(hypo_approx.number_of_increments):
         idx = (int(z_indices[0, col]),
                int(z_indices[1, col]),
                int(z_indices[2, col]),
                int(z_indices[3, col]))
-        if z_indices[1, col] < kevin_hypo.r_max:
+        if z_indices[1, col] < hypo_approx.bin_max.r:
             z_matrix[idx] += z_values[0, col]
 
-    print ('total number of photons in kevin z_matrix ='
-           ' %i (%.2f %%)'
-           %(z_matrix.sum(), z_matrix.sum()/my_hypo.tot_photons*100))
-    print ''
+    print('total number of photons in kevin z_matrix ='
+          ' %i (%.2f %%)'
+          %(z_matrix.sum(), z_matrix.sum() / hypo_ana_fast.tot_photons * 100))
 
-    print 'total_residual = ', (z - z_matrix).sum() / z.sum()
-    print ''
+    print('')
+
+    print('total_residual = ', (z - z_matrix).sum() / z.sum())
+
+    print('')
     #return
 
     # Create differential matrix
@@ -123,17 +118,16 @@ def main():
     cmap.set_under('w')
     cmap.set_bad('w')
 
-    # plot the track as a line
-    x_0, y_0, z_0 = my_hypo.track.point(my_hypo.track.t0)
-    #print 'track vertex', x_0, y_0, z_0
-    x_e, y_e, z_e = my_hypo.track.point(my_hypo.track.t0 + my_hypo.track.dt)
+    # Plot the track as a line
+    x_0, y_0, z_0 = hypo_ana_fast.track.point(hypo_ana_fast.track.t0)
+    x_e, y_e, z_e = hypo_ana_fast.track.point(hypo_ana_fast.track.t0
+                                              + hypo_ana_fast.track.dt)
     ax.plot([x_0, x_e], [y_0, y_e], zs=[z_0, z_e])
     ax.plot([-plt_lim, -plt_lim], [y_0, y_e], zs=[z_0, z_e], alpha=0.3, c='k')
     ax.plot([x_0, x_e], [plt_lim, plt_lim], zs=[z_0, z_e], alpha=0.3, c='k')
     ax.plot([x_0, x_e], [y_0, y_e], zs=[-plt_lim, -plt_lim], alpha=0.3, c='k')
 
-    # Make first plot
-    print 'Making first plot'
+    print('Making first plot')
     tt, yy = np.meshgrid(bin_edges.t, bin_edges.r)
     zz = z_diff.sum(axis=(2, 3))
     z_vmax = np.maximum(np.abs(np.min(zz)), np.max(zz))
@@ -172,11 +166,10 @@ def main():
     cb3.remove()
     cb4.remove()
 
-    # Make second plot
-    print 'Making second plot'
+    print('Making second plot')
     tt, yy = np.meshgrid(bin_edges.t, bin_edges.r)
     zz = z_per.sum(axis=(2, 3))
-    z_vmax = 5#np.maximum(np.abs(np.min(zz)), np.max(zz))
+    z_vmax = np.maximum(np.abs(np.min(zz)), np.max(zz))
     mg = ax2.pcolormesh(tt, yy, zz.T, vmin=-z_vmax, vmax=z_vmax, cmap=cmap)
     ax2.set_xlabel('t')
     ax2.set_ylabel('r')
@@ -184,7 +177,7 @@ def main():
 
     tt, yy = np.meshgrid(bin_edges.t, bin_edges.theta)
     zz = z_per.sum(axis=(1, 3))
-    z_vmax = 5#np.maximum(np.abs(np.min(zz)), np.max(zz))
+    z_vmax = np.maximum(np.abs(np.min(zz)), np.max(zz))
     mg = ax3.pcolormesh(tt, yy, zz.T, vmin=-z_vmax, vmax=z_vmax, cmap=cmap)
     ax3.set_xlabel('t')
     ax3.set_ylabel(r'$\theta$')
@@ -193,7 +186,7 @@ def main():
 
     tt, yy = np.meshgrid(bin_edges.t, bin_edges.phi)
     zz = z_per.sum(axis=(1, 2))
-    z_vmax = 5#np.maximum(np.abs(np.min(zz)), np.max(zz))
+    z_vmax = np.maximum(np.abs(np.min(zz)), np.max(zz))
     mg = ax4.pcolormesh(tt, yy, zz.T, vmin=-z_vmax, vmax=z_vmax, cmap=cmap)
     ax4.set_xlabel('t')
     ax4.set_ylabel(r'$\phi$')
@@ -218,8 +211,7 @@ def main():
     cmap.set_under('w')
     cmap.set_bad('w')
 
-    # Make third plot
-    print 'Making third plot'
+    print('Making third plot')
     tt, yy = np.meshgrid(bin_edges.t, bin_edges.r)
     zz = z_matrix.sum(axis=(2, 3))
     z_vmax = np.partition(zz.flatten(), -2)[-2]
@@ -258,8 +250,7 @@ def main():
     cb3.remove()
     cb4.remove()
 
-    # Make fourth plot
-    print 'Making fourth plot'
+    print('Making fourth plot')
     tt, yy = np.meshgrid(bin_edges.t, bin_edges.r)
     zz = z.sum(axis=(2, 3))
     z_vmax = np.partition(zz.flatten(), -2)[-2]