summary w/ median; attempt to fix coszen overflow; yapf cleanups

retro/generate_clsim_table.py
@@ -281,24 +281,35 @@ def generate_clsim_table(subdet, depth_idx, nevts, seed, tilt,
 
     t_min = 0 # ns
     r_min = 0 # meters
-    costheta_min, costheta_max = -1, 1
-    costhetadir_min, costhetadir_max = -1, 1
-    deltaphidir_min, deltaphidir_max = 0, np.pi # rad
+    costheta_min, costheta_max = -1.0, 1.0
+    costhetadir_min, costhetadir_max = -1.0, 1.0
+    deltaphidir_min, deltaphidir_max = 0.0, np.pi # rad
 
     r_binning_kw = dict(
-        min=float(r_min), max=float(r_max), n_bins=int(n_r_bins), power=int(r_power)
+        min=float(r_min),
+        max=float(r_max),
+        n_bins=int(n_r_bins),
+        power=int(r_power)
     )
     costheta_binning_kw = dict(
-        min=costheta_min, max=costheta_max, n_bins=n_costheta_bins
+        min=float(costheta_min),
+        max=float(costheta_max),
+        n_bins=int(n_costheta_bins)
     )
     t_binning_kw = dict(
-        min=t_min, max=t_max, n_bins=n_t_bins
+        min=float(t_min),
+        max=float(t_max),
+        n_bins=int(n_t_bins)
     )
     costhetadir_binning_kw = dict(
-        min=costhetadir_min, max=costhetadir_max, n_bins=n_costhetadir_bins
+        min=float(costhetadir_min),
+        max=float(costhetadir_max),
+        n_bins=int(n_costhetadir_bins)
     )
     deltaphidir_binning_kw = dict(
-        min=deltaphidir_min, max=deltaphidir_max, n_bins=n_deltaphidir_bins
+        min=float(deltaphidir_min),
+        max=float(deltaphidir_max),
+        n_bins=int(n_deltaphidir_bins)
     )
 
     axes = SphericalAxes([
@@ -312,7 +323,7 @@ def generate_clsim_table(subdet, depth_idx, nevts, seed, tilt,
         LinearAxis(**costhetadir_binning_kw),
         # deltaphidir: photon direction, (impact) azimuth angle (rad)
         LinearAxis(**deltaphidir_binning_kw)
-    ])
+    ]) # yapf: disable
 
     if subdet.lower() == 'ic':
         z_pos = IC_AVG_Z[depth_idx]
@@ -340,7 +351,7 @@ def generate_clsim_table(subdet, depth_idx, nevts, seed, tilt,
         IceModel='spice_mie',
         DisableTilt=not tilt,
         PhotonPrescale=1,
-        Sensor='none',
+        Sensor='none'
     )
 
     hashable_params = dict(

retro/plot_clsim_table_summary.py
@@ -64,14 +64,24 @@ def formatter(mapping, key_only=False, fname=False):
 
     """
     order = [
-        'hash_val', 'string', 'depth_idx', 'seed', 'table_shape',
-        'n_events', 'ice_model', 'tilt', 'n_photons', 'underflow', 'overflow'
-    ]
+        'hash_val',
+        'string',
+        'depth_idx',
+        'seed',
+        'table_shape',
+        'n_events',
+        'ice_model',
+        'tilt',
+        'n_photons',
+        'norm',
+        'underflow',
+        'overflow'
+    ] # yapf: disable
 
     line_sep = '\n'
 
     if fname:
-        for key in ['n_photons', 'underflow', 'overflow']:
+        for key in ('n_photons', 'norm', 'underflow', 'overflow'):
             order.remove(key)
 
     label_strs = []
@@ -92,13 +102,18 @@ def formatter(mapping, key_only=False, fname=False):
 
         if key == 'n_photons':
             label_strs.append(
-                'n_photons{}{}'
-                .format(sep, format_num(value, sigfigs=3, sci_thresh=(4, -3)))
+                '{}{}{}'.format(
+                    key, sep, format_num(value, sigfigs=3, sci_thresh=(4, -3))
+                )
             )
-        elif key in ['depth_idx', 'seed', 'string', 'n_events', 'ice_model', 'tilt']:
+
+        elif key in ('depth_idx', 'seed', 'string', 'n_events', 'ice_model',
+                     'tilt'):
             label_strs.append('{}{}{}'.format(key, sep, value))
+
         elif key == 'phase_refractive_index':
             label_strs.append('n{}{:.3f}'.format(sep, value))
+
         elif key in ('table_shape', 'underflow', 'overflow'):
             if key == 'table_shape':
                 name = 'shape'
@@ -126,6 +141,13 @@ def formatter(mapping, key_only=False, fname=False):
         elif key == 'hash_val':
             label_strs.append('hash{}{}'.format(sep, value))
 
+        elif key == 'norm':
+            label_strs.append(
+                '{}{}{}'.format(
+                    key, sep, format_num(value, sigfigs=3, sci_thresh=(4, -3))
+                )
+            )
+
     if not label_strs:
         return ''
 
@@ -145,6 +167,10 @@ def formatter(mapping, key_only=False, fname=False):
 def plot_clsim_table_summary(summaries, formats=None, outdir=None):
     """Plot the table summary produced by `summarize_clsim_table`.
 
+    Plots are made of marginalized 1D distributions, where mean, median, and/or
+    max are used to marginalize out the remaining dimensions (where those are
+    present in the summaries)..
+
     Parameters
     ----------
     summaries : string, summary, or iterable thereof
@@ -160,9 +186,9 @@ def plot_clsim_table_summary(summaries, formats=None, outdir=None):
 
     Returns
     -------
-    figs : list of two :class:`matplotlib.figure.Figure`
+    all_figs : list of three :class:`matplotlib.figure.Figure`
 
-    axes : list of to lists of :class:`matplotlib.axes.Axes`
+    all_axes : list of three lists of :class:`matplotlib.axes.Axes`
 
     summaries : list of :class:`collections.OrderedDict`
         List of all summaries loaded
@@ -244,7 +270,8 @@ def plot_clsim_table_summary(summaries, formats=None, outdir=None):
     depths = sorted(set(all_items['depth_idx']))
     seeds = sorted(set(all_items['seed']))
 
-    plot_kinds = ('mean', 'max')
+    plot_kinds = ('mean', 'median', 'max')
+    plot_kinds_with_data = set()
     dim_names = summaries[0]['dimensions'].keys()
     n_dims = len(dim_names)
 
@@ -254,18 +281,18 @@ def plot_clsim_table_summary(summaries, formats=None, outdir=None):
     fig_all_axes_y = n_dims * fig_one_axis_y
     fig_y = fig_header_y + fig_all_axes_y # inches
 
-    fig1, axes1 = plt.subplots(nrows=n_dims, ncols=1, squeeze=False,
-                               figsize=(fig_x, fig_y))
-    fig2, axes2 = plt.subplots(nrows=n_dims, ncols=1, squeeze=False,
-                               figsize=(fig_x, fig_y))
-    figs = [fig1, fig2]
-    axes1 = list(axes1.flat)
-    axes2 = list(axes2.flat)
-    axes = [axes1, axes2]
+    all_figs = []
+    all_axes = []
 
-    for ax_set in axes:
-        for ax in ax_set:
+    for plot_kind in plot_kinds:
+        fig, f_axes = plt.subplots(
+            nrows=n_dims, ncols=1, squeeze=False, figsize=(fig_x, fig_y)
+        )
+        all_figs.append(fig)
+        f_axes = list(f_axes.flat)
+        for ax in f_axes:
             ax.set_prop_cycle('color', COLOR_CYCLE_ORTHOG)
+        all_axes.append(f_axes)
 
     n_lines = 0
     xlims = [[np.inf, -np.inf]] * n_dims
@@ -291,7 +318,7 @@ def plot_clsim_table_summary(summaries, formats=None, outdir=None):
 
             for dim_num, dim_name in enumerate(dim_names):
                 dim_info = summary['dimensions'][dim_name]
-                dim_axes = (axes1[dim_num], axes2[dim_num])
+                dim_axes = [f_axes[dim_num] for f_axes in all_axes]
                 bin_edges = summary[dim_name + '_bin_edges']
                 if dim_name == 'deltaphidir':
                     bin_edges /= np.pi
@@ -300,6 +327,9 @@ def plot_clsim_table_summary(summaries, formats=None, outdir=None):
                     max(xlims[dim_num][1], np.max(bin_edges))
                 ]
                 for ax, plot_kind in zip(dim_axes, plot_kinds):
+                    if plot_kind not in dim_info:
+                        continue
+                    plot_kinds_with_data.add(plot_kind)
                     vals = dim_info[plot_kind]
                     ax.step(bin_edges, [vals[0]] + list(vals),
                             linewidth=1, clip_on=True,
@@ -326,8 +356,10 @@ def plot_clsim_table_summary(summaries, formats=None, outdir=None):
     if different_flabel:
         flabel += '__differ__' + different_flabel
 
-    for kind_idx, (plot_kind, fig) in enumerate(zip(plot_kinds, figs)):
-        for dim_num, (dim_name, ax) in enumerate(zip(dim_names, axes[kind_idx])):
+    for kind_idx, (plot_kind, fig) in enumerate(zip(plot_kinds, all_figs)):
+        if plot_kind not in plot_kinds_with_data:
+            continue
+        for dim_num, (dim_name, ax) in enumerate(zip(dim_names, all_axes[kind_idx])):
             #if dim_num == 0 and different_items:
             if different_items:
                 ax.legend(loc='best', frameon=False,
@@ -369,7 +401,7 @@ def plot_clsim_table_summary(summaries, formats=None, outdir=None):
             fig.savefig(outfpath, dpi=300)
             print('Saved image to "{}"'.format(outfpath))
 
-    return [fig1, fig2], [axes1, axes2], summaries
+    return all_figs, all_axes, summaries
 
 
 def parse_args(description=__doc__):

retro/summarize_clsim_table.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# pylint: disable=wrong-import-position, too-many-instance-attributes, too-many-locals
+# pylint: disable=wrong-import-position
 
 """
 Generate a json file summarizing a CLSim table
@@ -31,6 +31,7 @@ limitations under the License.'''
 from argparse import ArgumentParser
 from collections import OrderedDict
 from glob import glob
+from itertools import product
 from os.path import abspath, basename, dirname, isfile, join, splitext
 import sys
 from time import time
@@ -68,10 +69,10 @@ def summarize_clsim_table(table_fpath, table=None, save_summary=True,
 
     Returns
     -------
-    table : dictionary
+    table
         See `load_clsim_table` for details of the data structure
 
-    summary : dictionary
+    summary : OrderedDict
 
     """
     if save_summary:
@@ -107,11 +108,11 @@ def summarize_clsim_table(table_fpath, table=None, save_summary=True,
 
     summary = OrderedDict()
     for key in table.keys():
-        if key in ['table']:
+        if key == 'table':
             continue
         summary[key] = table[key]
     if fname_info:
-        for key in ['hash_val', 'string', 'depth_idx', 'seed']:
+        for key in ('hash_val', 'string', 'depth_idx', 'seed'):
             summary[key] = fname_info[key]
     # TODO: Add hole ice info when added to tray_kw_to_hash
     if meta:
@@ -142,9 +143,16 @@ def summarize_clsim_table(table_fpath, table=None, save_summary=True,
     )
     summary['norm'] = norm
 
-    dim_names = ['r', 'costheta', 't', 'costhetadir', 'deltaphidir']
+    dim_names = ('r', 'costheta', 't', 'costhetadir', 'deltaphidir')
     n_dims = len(table['table_shape'])
     assert n_dims == len(dim_names)
+
+    # Apply norm to underflow and overflow so magnitudes can be compared
+    # relative to plotted marginal distributions
+    for flow, idx in product(('underflow', 'overflow'), range(n_dims)):
+        summary[flow][idx] = summary[flow][idx] * norm
+
+    nonzero_table = np.ma.masked_equal(table['table'], 0)
 
     summary['dimensions'] = OrderedDict()
     for keep_axis, ax_name in zip(tuple(range(n_dims)), dim_names):
@@ -152,14 +160,21 @@ def summarize_clsim_table(table_fpath, table=None, save_summary=True,
         remove_axes.pop(keep_axis)
         remove_axes = tuple(remove_axes)
         axis = OrderedDict()
-        axis['mean'] = norm * np.mean(table['table'], axis=remove_axes)
-        axis['max'] = norm * np.max(table['table'], axis=remove_axes)
+        axis['mean'] = norm * np.asarray(
+            np.mean(table['table'], axis=remove_axes)
+        )
+        axis['median'] = norm * np.asarray(
+            np.ma.median(nonzero_table, axis=remove_axes)
+        )
+        axis['max'] = norm * np.asarray(
+            np.max(table['table'], axis=remove_axes)
+        )
         summary['dimensions'][ax_name] = axis
 
     if save_summary:
         ext = None
         base_fname = clsim_fname
-        while ext not in ['', '.fits']:
+        while ext not in ('', '.fits'):
             base_fname, ext = splitext(base_fname)
             ext = ext.lower()
         outfpath = join(outdir, base_fname + '_summary.json')

retro/table_readers.py
@@ -72,7 +72,7 @@ def load_clsim_table(fpath):
 
     Returns
     -------
-    table : dict
+    table : OrderedDict
         Items include
         - 'table' : np.ndarray
         - 'table_shape' : tuple of int
@@ -445,7 +445,7 @@ class CLSimTable(object):
 
             if isinstance(string, basestring):
                 self.string = string.strip().lower()
-                assert self.string in ['ic', 'dc']
+                assert self.string in ('ic', 'dc')
                 self.subdet = self.string
                 self.string_idx = None
 
@@ -486,7 +486,7 @@ class CLSimTable(object):
             self.depth_idx = info['depth_idx']
             self.seed = info['seed']
 
-        assert self.subdet in ['ic', 'dc']
+        assert self.subdet in ('ic', 'dc')
         self.dtp_fname_proto = RETRO_DOM_TABLE_FNAME_PROTO
 
         table_info = load_clsim_table(self.fpath)
@@ -546,7 +546,7 @@ class CLSimTable(object):
         # * Dividing by total photons thrown
         # * Dividing by (speed of light in ice, c / n, in m/ns
         #    multiplied by the bin width in ns; overall units are m)
-        # * Multiplying by a correction for angular acceptance, in [0,1]
+        # * Multiplying by a correction for angular acceptance, in [0, 1]
         # * Multiplying by the number of costheta bins (>= 1)
         # The result of applying this norm is in units of 1/meter, where
         # this unit accounts for the fact that CLSim tabulates the same
@@ -998,7 +998,7 @@ class TDICartTable(object):
 
             # Store the metadata by relative tile index
             rel_idx = tuple(int(np.round(i))
-                            for i in [x_float_idx, y_float_idx, z_float_idx])
+                            for i in (x_float_idx, y_float_idx, z_float_idx))
             to_load_meta[rel_idx] = meta
 
         x_min, y_min, z_min = lowermost_corner