2f2b274f0e6b2c9c341656e030d8d32c7bcbdb77
J.L. Lanfranchi <jll1062@phys.psu.edu>
2018-02-28 07:26:15 -0500

    don't weight during extraction; get gcd, mc_truth from i3 file

--- retro/i3processing/extract_photon_info.py
+++ retro/i3processing/extract_photon_info.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-# pylint: disable=wrong-import-position, wildcard-import, invalid-name
+# pylint: disable=wrong-import-position, wildcard-import, invalid-name, redefined-outer-name
 
 """
-Read in an i3 file from CLsim and generate a pickle file containing the photon
-info (a dict containing time, wavelength, coszen, and weight for each DOM).
+Read in an i3 file from CLsim and generate a pickle file containing the run
+parameters (gcd file, number of sims, and event MC truth) and photon info (a
+dict keyed by (string, dom) tuples containing time, wavelength, coszen, and
+weight for each DOM).
 """
 
 from __future__ import absolute_import, division, print_function
@@ -23,154 +25,166 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.'''
 
+from argparse import ArgumentParser
 from collections import OrderedDict
-from os.path import basename, dirname, expanduser, expandvars, join
+from os.path import expanduser, expandvars
 import sys
 import pickle
 import re
 
 import numpy as np
-from scipy.interpolate import interp1d
 
 from icecube import dataclasses, simclasses, dataio # pylint: disable=import-error, unused-import
 
-RETRO_DIR = dirname(dirname(__file__))
-if not RETRO_DIR:
-    RETRO_DIR = '../..'
 
-# dict to store the extracted info
-raw_data = OrderedDict([('doms', OrderedDict()), ('meta', OrderedDict())])
+def extract_photon_info(i3file, outfile=None, photons_key='photons'):
+    """Extract photon info (and metadata about the run) from an I3 file, and
+    store the info to a simple pickle file.
 
-fname = sys.argv[1]
+    Parameters
+    ----------
+    i3file : string
+        Path to the I3 file
 
-orig_fname = fname
-fname = expanduser(expandvars(fname))
-fname_base = basename(fname).rstrip('.bz2').rstrip('.i3')
-outfpath = fname_base + '_photon_raw_data.pkl'
-print('Extracting photons from "{}"\nand writing info to "{}"'
-      .format(orig_fname, outfpath))
+    outfile : string, optiional
+        If not provided, the output file name is the `i3file` but with
+        ".i3(.bz2)" extension(s) stripped and replaced with "_photons.pkl";
+        `outfile` will also be placed in the same directory as the `i3file`.
 
-i3_file = dataio.I3File(fname, 'r')
+    photons_key : string
+        Field name in the I3 frame that contains phton info.
 
-num_sims = 0
-while i3_file.more():
-    frame = i3_file.pop_frame()
-    if not frame.Has('photons'):
-        continue
+    Returns
+    -------
+    photon_info
 
-    num_sims += 1
-    if num_sims % 100 == 0:
-        sys.stdout.write('.')
-        sys.stdout.flush()
+    """
+    # dict to store the extracted info
+    photon_info = OrderedDict([
+        ('gcd', None),
+        ('mc_truth', None),
+        ('num_sims', 0),
+        ('doms', OrderedDict())
+    ])
 
-    photon_series = frame['photons']
+    orig_fname = i3file
+    fname = expanduser(expandvars(i3file))
 
-    for dom_key, photon_info in photon_series:
-        if dom_key not in raw_data['doms']:
-            raw_data['doms'][dom_key] = OrderedDict([
-                ('time', []),
-                ('wavelength', []),
-                ('coszen', []),
-            ])
+    if outfile is None:
+        outfile = re.sub(r'\.i3.*', '', fname) + '_photons.pkl'
 
-        for photon in photon_info:
-            raw_data['doms'][dom_key]['time'].append(photon.time)
-            raw_data['doms'][dom_key]['wavelength'].append(photon.wavelength)
-            raw_data['doms'][dom_key]['coszen'].append(np.cos(photon.dir.zenith))
+    print('Extracting photons from "{}"\nand writing info to "{}"'
+          .format(orig_fname, outfile))
 
-if num_sims >= 100:
-    sys.stdout.write('\n')
+    i3_file = dataio.I3File(fname, 'r')
+
+    num_sims = 0
+    mc_truth = None
+    while i3_file.more():
+        if photon_info['gcd'] is None:
+            frame = i3_file.pop_frame()
+        else:
+            frame = i3_file.pop_daq()
+
+        if frame.Stop == frame.TrayInfo:
+            for config in frame.values():
+                try:
+                    streams = config.module_configs['streams']
+                    gcd = streams['Prefix']
+                except (KeyError, ValueError, AttributeError) as e:
+                    print(e)
+                    continue
+                gcd_l = gcd.lower()
+                if not ('gcd' in gcd_l or 'geocalibdetector' in gcd_l):
+                    continue
+                photon_info['gcd'] = gcd
+
+        if photons_key not in frame:
+            continue
+
+        num_sims += 1
+        if num_sims % 100 == 0:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+
+        if mc_truth is None:
+            mct = frame['I3MCTree']
+            primary = mct.get_primaries()[0]
+            mc_truth = OrderedDict()
+            mc_truth['pdg'] = int(primary.pdg_encoding)
+            mc_truth['t'] = float(primary.time)
+            mc_truth['x'] = float(primary.pos.x)
+            mc_truth['y'] = float(primary.pos.y)
+            mc_truth['z'] = float(primary.pos.z)
+            mc_truth['zenith'] = float(primary.dir.zenith)
+            mc_truth['azimuth'] = float(primary.dir.azimuth)
+            mc_truth['energy'] = float(primary.energy)
+
+        photon_series = frame['photons']
+
+        for dom_key, pinfo in photon_series:
+            if dom_key not in photon_info['doms']:
+                photon_info['doms'][dom_key] = OrderedDict([
+                    ('time', []),
+                    ('wavelength', []),
+                    ('coszen', []),
+                ])
+
+            for photon in pinfo:
+                photon_info['doms'][dom_key]['time'].append(photon.time)
+                photon_info['doms'][dom_key]['wavelength'].append(photon.wavelength)
+                photon_info['doms'][dom_key]['coszen'].append(np.cos(photon.dir.zenith))
+
+    photon_info['mc_truth'] = mc_truth
+    photon_info['num_sims'] = num_sims
+
+    if num_sims >= 100:
+        sys.stdout.write('\n')
+
+    # Convert I3OMKey object to integers so we can ditch the albatross known as
+    # IceCube software
+    photon_info['doms'] = {
+        (int(k.string), int(k.om)): v for k, v in photon_info['doms'].items()
+    }
+
+    # Sort dict by (string, dom) such that output file is guaranteed to be
+    # consistent
+    sorted_data = OrderedDict()
+    for dom in sorted(photon_info['doms'].keys()):
+        data_dict = photon_info['doms'][dom]
+        for k in data_dict.keys():
+            data_dict[k] = np.array(data_dict[k], dtype=np.float32)
+        sorted_data[dom] = data_dict
+    photon_info['doms'] = sorted_data
+
+    pickle.dump(photon_info,
+                open(outfile, 'wb'),
+                protocol=pickle.HIGHEST_PROTOCOL)
+
+    return photon_info
 
 
-# Convert I3OMKey object to integers so we can ditch the albatross known as
-# IceCube software
-raw_data['doms'] = {
-    (int(k.string), int(k.om)): v for k, v in raw_data['doms'].items()
-}
-
-# Sorted dict by (string, dom) such that output file is guaranteed to be
-# consistent
-sorted_data = OrderedDict()
-for dom in sorted(raw_data['doms'].keys()):
-    data_dict = raw_data['doms'][dom]
-    for k in data_dict.keys():
-        data_dict[k] = np.array(data_dict[k])
-    sorted_data[dom] = data_dict
-raw_data['doms'] = sorted_data
-
-#==============================================================================
-# Add weights that transform the samples into a realistic distribution of
-# photon detection probabilities
-#==============================================================================
-
-# Since we assume the generated photons will account for absolute energy
-# contained within the "useful" range of wavelengths, we normalize the Ckv
-# wavelength distribution to peak at 1 and all other probabilities are reduced
-# from that. "Overall" normalization factor is, therefore, contained in the
-# number of photons a hypothesis says it produces.
-
-# Shortest wavelength (and therefore the point at which to normalize to 1) is
-# assumed to be 260 nm.
-wl_ckv_accept = np.loadtxt(
-    join(
-        RETRO_DIR,
-        'data',
-        'sampled_cherenkov_distr_and_dom_acceptance_vs_wavelength.csv'
-    ),
-    delimiter=','
-)
-wavelengths = wl_ckv_accept[:, 0]
-ckv_distr = wl_ckv_accept[:, 1]
-ckv_distr_linterp = interp1d(
-    x=wavelengths,
-    y=ckv_distr,
-    kind='linear'
-)
+def parse_args(description=__doc__):
+    """Parse command line arguments"""
+    parser = ArgumentParser(description=description)
+    parser.add_argument(
+        '--photons-key', default='photons',
+        help='''Field in the I3 frame that identifies photons. Default is
+        "photons"'''
+    )
+    parser.add_argument(
+        '--outfile', default=None,
+        help='''Path to pickle file in which results are stored. If not
+        specified, the name will be the input file with the ".i3(.bz2)"
+        extension(s) replaced by "_photons.pkl" (and placed in same
+        directory as the input file).'''
+    )
+    parser.add_argument(
+        'i3file',
+        help='Path to the I3 file from which you wish to extract photon info'
+    )
+    return parser.parse_args()
 
 
-match = re.match(r'.*holeice_([^_]*)', fname_base)
-if match is None:
-    print('Could not find hole ice model in filename; using "as.h2-50cm"')
-    hole_ice_model = 'as.h2-50cm'
-else:
-    hole_ice_model = match.groups()[0]
-
-# Note the first number in the file is a number approximately equal (but
-# greater than) the peak in the distribution, so is useless for us.
-poly_coeffs = np.loadtxt(expandvars(
-    '$I3_SRC/ice-models/resources/models/angsens/' + hole_ice_model
-))[1:]
-
-# We want coszen = -1 to correspond to upgoing particles, but angular
-# sensitivity is given w.r.t. the DOM axis (which points "down" towards earth,
-# and therefore is rotated 180-deg). So mirror the coszen polynomial about cz=0
-# by negating the odd coefficients.
-flipped_coeffs = np.empty_like(poly_coeffs)
-flipped_coeffs[0::2] = poly_coeffs[0::2]
-flipped_coeffs[1::2] = -poly_coeffs[1::2]
-
-angsens_poly = np.polynomial.Polynomial(flipped_coeffs, domain=(-1, 1))
-
-
-for key, data_dict in raw_data['doms'].items():
-    cz = data_dict['coszen']
-    try:
-        # Note that angular sensitivity _will_ modify the total number of
-        # photons detected, so we do not divide by the sum of weights here.
-        angsens_wt = angsens_poly(cz)
-    except:
-        print(np.min(cz), np.max(cz))
-        raise
-
-    #data_dict['weight'] = ckv_wt * angsens_wt / num_sims
-    data_dict['weight'] = angsens_wt / num_sims
-
-    for k, array in data_dict.items():
-        data_dict[k] = array.astype(np.float32)
-
-raw_data['meta']['num_sims'] = num_sims
-pickle.dump(
-    raw_data,
-    open(outfpath, 'wb'),
-    protocol=pickle.HIGHEST_PROTOCOL
-)
+if __name__ == '__main__':
+    photon_info = extract_photon_info(**vars(parse_args()))
