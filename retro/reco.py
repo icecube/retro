b464510336b4e5c9178fb5e85418e0435c3a6588
Philipp Eller <peller@phys.ethz.ch>
2018-04-26 12:07:52 -0400

    separating out priors into module

--- retro/reco.py
+++ retro/reco.py
@@ -55,167 +55,7 @@ from retro import init_obj
 from retro.const import TWO_PI, ALL_STRS_DOMS_SET, EMPTY_SOURCES
 from retro.retro_types import PARAM_NAMES
 from retro.utils.misc import expand, mkdir, sort_dict
-
-
-PRI_UNIFORM = 'uniform'
-PRI_LOG_UNIFORM = 'log_uniform'
-PRI_LOG_NORMAL = 'log_normal'
-PRI_COSINE = 'cosine'
-PRI_GAUSSIAN = 'gaussian'
-PRI_SPEFIT2 = 'spefit2'
-PRI_CAUCHY = 'cauchy'
-
-
-def get_prior_fun(param, reco_kw):
-    '''
-    Function to generate prior definitions from kw_args
-    Should go into its own module
-    '''
-
-    if 'zenith' in param:
-        return (PRI_COSINE, (-1, 1))
-
-    elif 'azimuth' in param:
-        return (PRI_UNIFORM, (0, TWO_PI))
-
-    elif param == 'x':
-        spatial_prior_orig = reco_kw.get('spatial_prior').strip()
-        spatial_prior_name = spatial_prior_orig.lower()
-        if spatial_prior_name == 'ic':
-            return (PRI_UNIFORM, (-860, 870))
-        elif spatial_prior_name == 'dc':
-            return (PRI_UNIFORM, (-150, 270))
-        elif spatial_prior_name == 'dc_subdust':
-            return (PRI_UNIFORM, (-150, 270))
-        elif spatial_prior_name == 'spefit2':
-            return (
-                PRI_SPEFIT2,
-                (
-                    # scipy.stats.cauchy loc, scale parameters
-                    -0.19687812829978152, 14.282171566308806,
-                    # Hard limits
-                    -600, 750
-                )
-            )
-        else:
-            raise ValueError('Spatial prior "{}" not recognized'
-                             .format(spatial_prior_orig))
-
-    elif param == 'y':
-        spatial_prior_orig = reco_kw.get('spatial_prior').strip()
-        spatial_prior_name = spatial_prior_orig.lower()
-        if spatial_prior_name == 'ic':
-            return (PRI_UNIFORM, (-780, 770))
-        elif spatial_prior_name == 'dc':
-            return (PRI_UNIFORM, (-210, 150))
-        elif spatial_prior_name == 'dc_subdust':
-            return (PRI_UNIFORM, (-210, 150))
-        elif spatial_prior_name == 'spefit2':
-            return (
-                PRI_SPEFIT2,
-                (
-                    # scipy.stats.cauchy loc, scale parameters
-                    -0.2393645701205161, 15.049528023495354,
-                    # Hard limits
-                    -750, 650
-                )
-            )
-        else:
-            raise ValueError('Spatial prior "{}" not recognized'
-                             .format(spatial_prior_orig))
-
-
-    elif param == 'z':
-        spatial_prior_orig = reco_kw.get('spatial_prior').strip()
-        spatial_prior_name = spatial_prior_orig.lower()
-        if spatial_prior_name == 'ic':
-            return (PRI_UNIFORM, (-780, 790))
-        elif spatial_prior_name == 'dc':
-            return (PRI_UNIFORM, (-770, 760))
-        elif spatial_prior_name == 'dc_subdust':
-            return (PRI_UNIFORM, (-610, -60))
-        elif spatial_prior_name == 'spefit2':
-            return (
-                PRI_SPEFIT2,
-                (
-                    # scipy.stats.cauchy loc, scale parameters
-                    -5.9170661027492546, 12.089399308036718,
-                    # Hard limits
-                    -1200, 200
-                )
-            )
-        else:
-            raise ValueError('Spatial prior "{}" not recognized'
-                             .format(spatial_prior_orig))
-
-
-
-    elif param == 'time':
-        temporal_prior_orig = reco_kw.pop('temporal_prior').strip()
-        temporal_prior_name = temporal_prior_orig.lower()
-        if temporal_prior_name == PRI_UNIFORM:
-            return (PRI_UNIFORM, (-4e3, 0.0))
-        elif temporal_prior_name == PRI_SPEFIT2:
-            return (
-                PRI_SPEFIT2,
-                (
-                    # scipy.stats.cauchy loc (rel to SPEFit2 time), scale
-                    -82.631395081663754, 75.619895703067343,
-                    # Hard limits (relative to left, right edges of window,
-                    # respectively)
-                    -4e3, 0.0
-                )
-            )
-        else:
-            raise ValueError('Temporal prior "{}" not recognized'
-                             .format(temporal_prior_orig))
-
-    elif param == 'cascade_energy':
-        cascade_energy_prior_name = reco_kw.pop('cascade_energy_prior')
-        cascade_energy_lims = reco_kw.pop('cascade_energy_lims')
-        if cascade_energy_prior_name == PRI_UNIFORM:
-            return (PRI_UNIFORM, (np.min(cascade_energy_lims), np.max(cascade_energy_lims)))
-        elif cascade_energy_prior_name == PRI_LOG_UNIFORM:
-            return (PRI_LOG_UNIFORM, (np.min(cascade_energy_lims), np.max(cascade_energy_lims)))
-        elif cascade_energy_prior_name == PRI_LOG_NORMAL:
-            return (
-                PRI_LOG_NORMAL,
-                (
-                    # scipy.stats.lognorm 3 paramters
-                    0.96251341305506233, 0.4175592980195757, 17.543915051586644,
-                    # hard limits
-                    np.min(cascade_energy_lims), np.max(cascade_energy_lims)
-                )
-            )
-        else:
-            raise ValueError(str(cascade_energy_prior_name))
-
-    elif param == 'track_energy':
-        track_energy_prior_name = reco_kw.pop('track_energy_prior')
-        track_energy_lims = reco_kw.pop('track_energy_lims')
-        if track_energy_prior_name == PRI_UNIFORM:
-            return (PRI_UNIFORM, (np.min(track_energy_lims), np.max(track_energy_lims)))
-        elif track_energy_prior_name == PRI_LOG_UNIFORM:
-            return (PRI_LOG_UNIFORM, (np.min(track_energy_lims), np.max(track_energy_lims)))
-        elif track_energy_prior_name == PRI_LOG_NORMAL:
-            return (
-                PRI_LOG_NORMAL,
-                (
-                    # scipy.stats.lognorm 3 paramters
-                    0.96251341305506233, 0.4175592980195757, 17.543915051586644,
-                    # hard limits
-                    np.min(track_energy_lims), np.max(track_energy_lims)
-                )
-            )
-        else:
-            raise ValueError(str(track_energy_prior_name))
-
-    else:
-        raise ValueError('Dimension %s unknown for priors'%param)
-
-
-
-
+from retro.priors import *
 
 
 class retro_reco(object):
@@ -233,9 +73,9 @@ class retro_reco(object):
 
 
         # setup priors
-        self.priors = OrderedDict()
+        self.prior_defs = OrderedDict()
         for param in self.hypo_handler.params:
-            self.priors[param] = get_prior_fun(param, reco_kw)
+            self.prior_defs[param] = get_prior_def(param, reco_kw)
         # keyword fuckery
         reco_kw.pop('spatial_prior')
 
@@ -323,75 +163,11 @@ class retro_reco(object):
 
         prior_funcs = []
         for dim_num, dim_name in enumerate(hypo_params):
-            prior_kind, prior_params = self.priors[dim_name]
-            if prior_kind is PRI_UNIFORM:
-                # Time is special since prior is relative to hits in the event
-                if dim_name == 'time':
-                    prior_params = (
-                        hits_summary['earliest_hit_time'] + prior_params[0],
-                        hits_summary['latest_hit_time'] + prior_params[1]
-                    )
-                priors_used[dim_name] = (prior_kind, prior_params)
 
-                if prior_params == (0, 1):
-                    continue
-                    #def prior_func(cube): # pylint: disable=unused-argument
-                    #    pass
-                elif np.min(prior_params[0]) == 0:
-                    maxval = np.max(prior_params)
-                    def prior_func(cube, n=dim_num, maxval=maxval):
-                        cube[n] = cube[n] * maxval
-                else:
-                    minval = np.min(prior_params)
-                    width = np.max(prior_params) - minval
-                    def prior_func(cube, n=dim_num, width=width, minval=minval):
-                        cube[n] = cube[n] * width + minval
+            prior_fun, prior_def = get_prior_fun(dim_num, dim_name, self.prior_defs[dim_name], event)
+            prior_funcs.append(prior_fun)
+            priors_used[dim_name] = prior_def
 
-            elif prior_kind == PRI_LOG_UNIFORM:
-                priors_used[dim_name] = (prior_kind, prior_params)
-                log_min = np.log(np.min(prior_params))
-                log_width = np.log(np.max(prior_params) / np.min(prior_params))
-                def prior_func(cube, n=dim_num, log_width=log_width, log_min=log_min):
-                    cube[n] = exp(cube[n] * log_width + log_min)
-
-            elif prior_kind == PRI_COSINE:
-                priors_used[dim_name] = (prior_kind, prior_params)
-                cos_min = np.min(prior_params)
-                cos_width = np.max(prior_params) - cos_min
-                def prior_func(cube, n=dim_num, cos_width=cos_width, cos_min=cos_min):
-                    cube[n] = acos(cube[n] * cos_width + cos_min)
-
-            elif prior_kind == PRI_GAUSSIAN:
-                priors_used[dim_name] = (prior_kind, prior_params)
-                mean, stddev = prior_params
-                norm = 1 / (stddev * np.sqrt(TWO_PI))
-                def prior_func(cube, n=dim_num, norm=norm, mean=mean, stddev=stddev):
-                    cube[n] = norm * exp(-((cube[n] - mean) / stddev)**2)
-
-            elif prior_kind == PRI_LOG_NORMAL:
-                priors_used[dim_name] = (prior_kind, prior_params)
-                shape, loc, scale, low, high = prior_params
-                lognorm = stats.lognorm(shape, loc, scale)
-                def prior_func(cube, lognorm=lognorm, n=dim_num, low=low, high=high):
-                    cube[n] = np.clip(lognorm.isf(cube[n]), a_min=low, a_max=high)
-
-            elif prior_kind == PRI_SPEFIT2:
-                spe_fit_val = event['recos']['SPEFit2'][dim_name]
-                rel_loc, scale, low, high = prior_params
-                loc = spe_fit_val + rel_loc
-                cauchy = stats.cauchy(loc=loc, scale=scale)
-                if dim_name == 'time':
-                    low += hits_summary['time_window_start']
-                    high += hits_summary['time_window_stop']
-                priors_used[dim_name] = (PRI_CAUCHY, (loc, scale, low, high))
-                def prior_func(cube, cauchy=cauchy, n=dim_num, low=low, high=high):
-                    cube[n] = np.clip(cauchy.isf(cube[n]), a_min=low, a_max=high)
-
-            else:
-                raise NotImplementedError('Prior "{}" not implemented.'
-                                          .format(prior_kind))
-
-            prior_funcs.append(prior_func)
 
         param_values = []
         log_likelihoods = []
@@ -513,7 +289,7 @@ class retro_reco(object):
 
         mn_meta = OrderedDict([
             ('params', hypo_params),
-            ('original_prior_specs', self.priors),
+            ('original_prior_specs', self.prior_defs),
             ('priors_used', priors_used),
             ('time_window', time_window),
             ('kwargs', sort_dict(mn_kw)),
