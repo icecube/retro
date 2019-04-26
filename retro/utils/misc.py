fb4e7cbcebf39de0ea946ccd40399662545184dc
J.L. Lanfranchi <jll1062@phys.psu.edu>
2019-04-26 15:47:37 -0400

    get rid of prints

--- retro/utils/misc.py
+++ retro/utils/misc.py
@@ -885,10 +885,8 @@ def join_struct_arrays(arrays):
     sizes = np.array([a.itemsize for a in arrays])
     offsets = np.r_[0, sizes.cumsum()]
     n = len(arrays[0])
-    print("len(arrays):", len(arrays), "len(arrays[0]):", n, "sizes:", sizes, "offsets:", offsets)
     joint = np.empty((n, offsets[-1]), dtype=np.uint8)
     for a, size, offset in zip(arrays, sizes, offsets):
-        print(size, offset)
         joint[:, offset : offset + size] = a.view(np.uint8).reshape(n, size)
     dtype = sum((a.dtype.descr for a in arrays), [])
     return joint.ravel().view(dtype)
